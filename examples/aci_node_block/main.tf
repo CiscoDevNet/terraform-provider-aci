349ee72da82b642fb54cb0d91b6bb2a1ebadda9f
Ashutosh Soni <ashutosh.soni@CDSYS.LOCAL>
2020-06-15 17:47:20 +0530

    CACIT-46 added example and updated aci.erb

---
+++ examples/aci_node_block/main.tf
@@ -0,0 +1,18 @@
+resource "aci_leaf_profile" "checkBLK" {
+  name        = "example"
+}
+
+resource "aci_switch_association" "checkBLK" {
+  leaf_profile_dn  = "${aci_leaf_profile.checkBLK.id}"
+  name  = "example"
+  switch_association_type  = "range"
+}
+
+resource "aci_node_block" "check" {
+  switch_association_dn   = "${aci_switch_association.checkBLK.id}"
+  name                    = "block"
+  annotation              = "aci_node_block"
+  from_                   = "101"
+  name_alias              = "node_block"
+  to_                     = "106"
+}
