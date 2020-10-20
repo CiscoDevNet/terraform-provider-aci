85c4a4d317179d17768eac1a2693f5f175bfdb4c
nkatarmal-crest <nirav.katarmal@crestdatasys.com>
2020-10-20 17:15:29 +0530

    formatting done

--- examples/client_end_point/main.tf
+++ examples/client_end_point/main.tf
@@ -6,20 +6,20 @@ provider "aci" {
 }
 
 data "aci_tenant" "tenant" {
-    name = "tenant"
+  name = "tenant"
 }
 
 data "aci_application_profile" "ap" {
-    tenant_dn = "${data.aci_tenant.tenant.id}"
-    name = "ap1"
+  tenant_dn = "${data.aci_tenant.tenant.id}"
+  name      = "ap1"
 }
 
 data "aci_application_epg" "epg" {
   application_profile_dn = "${data.aci_application_profile.ap.id}"
-  name = "checkepg"
+  name                   = "checkepg"
 }
 
 data "aci_client_end_point" "example" {
-  application_epg_dn  = "${data.aci_application_epg.epg.id}"
-  name                = "example"
+  application_epg_dn = "${data.aci_application_epg.epg.id}"
+  name               = "example"
 }
