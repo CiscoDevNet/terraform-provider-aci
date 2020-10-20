fa6301f8300dc9896a3e8bebbcde4d4b85bfccad
nkatarmal-crest <nirav.katarmal@crestdatasys.com>
2020-10-20 11:40:17 +0530

    CACIT-84 cloudSubnet zone update

--- examples/cloud_ctx_profile/main.tf
+++ examples/cloud_ctx_profile/main.tf
@@ -5,11 +5,20 @@ provider "aci" {
   insecure = true
 }
 
-# provider "aci" {
-#   username    = ""
-#   private_key = ""
-#   cert_name   = ""
-#   url         = ""
-#   insecure    = true
-# }
+resource "aci_tenant" "tenentcheck" {
+  description = "cloud resources"
+  name        = "cloudTenant"
+  annotation  = "cloudTenant"
+  name_alias  = "cloud_tenant"
+}
 
+resource "aci_cloud_context_profile" "ctx1" {
+	name 		                  = "check"
+	description               = "cloud_context_profile created while acceptance testing"
+	tenant_dn                 = "${aci_tenant.tenentcheck.id}"
+	primary_cidr              = "10.0.0.0/16"
+	region                    = "us-west-1"
+	cloud_vendor			        = "aws"
+	relation_cloud_rs_to_ctx  = "${aci_vrf.vrf1.id}"
+  hub_network               =  "uni/tn-infra/gwrouterp-default"
+}
