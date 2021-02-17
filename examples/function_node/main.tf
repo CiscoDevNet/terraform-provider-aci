5dda1672c8e827fae93bbfbafd98d05bb72f17cb
nkatarmal-crest <nirav.katarmal@crestdatasys.com>
2021-02-17 15:57:39 +0530

    added examples of some resources

---
+++ examples/function_node/main.tf
@@ -0,0 +1,20 @@
+
+provider "aci" {
+  username = ""
+  password = ""
+  url      = ""
+  insecure = true
+}
+
+resource "aci_function_node" "foofunction_node" {
+
+  l4_l7_service_graph_template_dn = aci_l4_l7_service_graph_template.serviceGraphTemp.id
+  name                            = "functionNodeOne"
+  func_template_type              = "OTHER"
+  func_type                       = "None"
+  is_copy                         = "no"
+  managed                         = "yes"
+  routing_mode                    = "unspecified"
+  sequence_number                 = "3"
+  share_encap                     = "yes"
+}
---
+++ examples/l4_l7_service_graph_template/main.tf
@@ -0,0 +1,15 @@
+provider "aci" {
+  username = ""
+  password = ""
+  url      = ""
+  insecure = true
+}
+
+resource "aci_l4_l7_service_graph_template" "example" {
+  tenant_dn                         = "${aci_tenant.tenentcheck.id}"
+  name                              = "second"
+  name_alias                        = "alias"
+  description                       = "from terraform"
+  l4_l7_service_graph_template_type = "cloud"
+  ui_template_type                  = "ONE_NODE_ADC_ONE_ARM"
+}
