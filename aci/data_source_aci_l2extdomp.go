a6370a205bcc640aa38a3ee92fdec910b07310f1
Ashutosh Soni <74891251+AshuSoni-crest@users.noreply.github.com>
2021-02-20 14:35:18 +0530

    469 l2extDomP aci_l2_domain (#249)
    resource file of l2extDomP

---
+++ aci/data_source_aci_l2extdomp.go
@@ -0,0 +1,56 @@
+package aci
+
+import (
+	"fmt"
+
+	"github.com/ciscoecosystem/aci-go-client/client"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceAciL2Domain() *schema.Resource {
+	return &schema.Resource{
+
+		Read: dataSourceAciL2DomainRead,
+
+		SchemaVersion: 1,
+
+		Schema: AppendBaseAttrSchema(map[string]*schema.Schema{
+
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+
+			"annotation": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"name_alias": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		}),
+	}
+}
+
+func dataSourceAciL2DomainRead(d *schema.ResourceData, m interface{}) error {
+	aciClient := m.(*client.Client)
+
+	name := d.Get("name").(string)
+
+	rn := fmt.Sprintf("l2dom-%s", name)
+
+	dn := fmt.Sprintf("uni/%s", rn)
+
+	l2extDomP, err := getRemoteL2Domain(aciClient, dn)
+
+	if err != nil {
+		return err
+	}
+	d.SetId(dn)
+	setL2DomainAttributes(l2extDomP, d)
+	return nil
+}
