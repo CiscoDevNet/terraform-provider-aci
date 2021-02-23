3ad8d153b8fb719536109ff81fc5576eefeace59
cindy zhao <xinyuezhao96@gmail.com>
2021-02-23 12:31:21 -0800

    Add examples

---
+++ examples/aci_endpoint_security_group/rest.tf
@@ -0,0 +1,8 @@
+resource "aci_rest" "rest_cons_if" {
+  path       = "api/node/mo/${aci_endpoint_security_group.terraform_esg.id}/rsconsIf-interface.json"
+  class_name = "fvRsConsIf"
+
+  content = {
+    "name" = "interface"
+  }
+}
---
+++ examples/aci_endpoint_security_group/versions.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_providers {
+    aci = {
+      source = "ciscodevnet/aci"
+    }
+  }
+  required_version = ">= 0.13"
+}
