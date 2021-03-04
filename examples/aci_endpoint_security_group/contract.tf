a745f83a85bf830e9c9dabea21d84d6e28baf589
cindy zhao <xinyuezhao96@gmail.com>
2021-03-04 11:40:25 -0800

    Add examples for EESG

--- examples/aci_endpoint_security_group/contract.tf
+++ examples/aci_endpoint_security_group/contract.tf
@@ -1,31 +1,33 @@
-resource "aci_tenant" "tenant_for_contract" {
-  name        = "tenant_for_contract"
-  description = "This tenant is created by terraform ACI provider"
-}
-
 resource "aci_contract" "rs_prov_contract" {
-  tenant_dn                = aci_tenant.tenant_for_contract.id
+  tenant_dn                = aci_tenant.terraform_tenant.id
   name                     = "rs_prov_contract"
   description              = "This contract is created by terraform ACI provider"
-  scope                    = "context"
+  scope                    = "tenant"
   target_dscp              = "VA"
   prio                     = "unspecified"
 }
 
 resource "aci_contract" "rs_cons_contract" {
-  tenant_dn                = aci_tenant.tenant_for_contract.id
+  tenant_dn                = aci_tenant.terraform_tenant.id
   name                     = "rs_cons_contract"
   description              = "This contract is created by terraform ACI provider"
-  scope                    = "context"
+  scope                    = "tenant"
   target_dscp              = "VA"
   prio                     = "unspecified"
 }
 
 resource "aci_contract" "intra_epg_contract" {
-  tenant_dn                = aci_tenant.tenant_for_contract.id
+  tenant_dn                = aci_tenant.terraform_tenant.id
   name                     = "intra_epg_contract"
   description              = "This contract is created by terraform ACI provider"
-  scope                    = "context"
+  scope                    = "tenant"
   target_dscp              = "VA"
   prio                     = "unspecified"
 }
+
+resource "aci_contract" "exported_contract" {
+  tenant_dn                = aci_tenant.tenant_export_cons.id
+  name                     = "exported_contract"
+  description              = "This contract is exported to tenant_export_cons"
+}
+
