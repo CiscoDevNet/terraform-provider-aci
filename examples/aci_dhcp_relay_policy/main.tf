a76021e97b91d7d01a7f84e8f879f038a33c0ff4
AshuSoni-crest <ashutosh.soni@crestdatasys.com>
2021-03-10 15:23:35 +0530

    added examples of new resources

---
+++ examples/aci_dhcp_relay_policy/main.tf
@@ -0,0 +1,16 @@
+
+provider "aci" {
+  username = ""
+  password = ""
+  url      = ""
+  insecure = true
+}
+
+resource "aci_dhcp_relay_policy" "example" {
+  tenant_dn  = aci_tenant.example.id
+  name  = "name_example"
+  annotation  = "annotation_example"
+  mode  = "visible"
+  name_alias  = "alias_example"
+  owner  = "infra"
+}
