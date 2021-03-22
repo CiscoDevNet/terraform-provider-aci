7552d84767e5b2171ea02d696fa8548bc0646382
Ashutosh Soni <74891251+AshuSoni-crest@users.noreply.github.com>
2021-03-22 12:14:34 +0530

    CACIT-1755 added resource and data-source of hsrpGroupPol (#312)

---
+++ aci/data_source_aci_hsrpgrouppol.go
@@ -0,0 +1,121 @@
+package aci
+
+import (
+	"fmt"
+
+	"github.com/ciscoecosystem/aci-go-client/client"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceAciHSRPGroupPolicy() *schema.Resource {
+	return &schema.Resource{
+
+		Read: dataSourceAciHSRPGroupPolicyRead,
+
+		SchemaVersion: 1,
+
+		Schema: AppendBaseAttrSchema(map[string]*schema.Schema{
+			"tenant_dn": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+
+			"annotation": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"ctrl": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"hello_intvl": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"hold_intvl": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"key": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"name_alias": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"preempt_delay_min": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"preempt_delay_reload": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"preempt_delay_sync": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"prio": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"timeout": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"hsrp_group_policy_type": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		}),
+	}
+}
+
+func dataSourceAciHSRPGroupPolicyRead(d *schema.ResourceData, m interface{}) error {
+	aciClient := m.(*client.Client)
+
+	name := d.Get("name").(string)
+
+	rn := fmt.Sprintf("hsrpGroupPol-%s", name)
+	TenantDn := d.Get("tenant_dn").(string)
+
+	dn := fmt.Sprintf("%s/%s", TenantDn, rn)
+
+	hsrpGroupPol, err := getRemoteHSRPGroupPolicy(aciClient, dn)
+
+	if err != nil {
+		return err
+	}
+	d.SetId(dn)
+	setHSRPGroupPolicyAttributes(hsrpGroupPol, d)
+	return nil
+}
