ce756d16e0fc5d1b20c605ee6d3b2f6c2966f8b7
AshuSoni-crest <ashutosh.soni@crestdatasys.com>
2021-03-30 16:00:41 +0530

    added examples of sprint-3 resources

---
+++ examples/l3out_path_attachment_secondary_ip/main.tf
@@ -0,0 +1,35 @@
+provider "aci" {
+  username = ""
+  password = ""
+  url      = ""
+  insecure = true
+}
+
+resource "aci_l3out_path_attachment" "example" {
+
+  logical_interface_profile_dn = aci_logical_interface_profile.example.id
+  target_dn                    = "topology/pod-1/paths-101/pathep-[eth1/1]"
+  if_inst_t                    = "ext-svi"
+  addr                         = "0.0.0.0"
+  annotation                   = "example"
+  autostate                    = "disabled"
+  encap                        = "vlan-1"
+  encap_scope                  = "ctx"
+  ipv6_dad                     = "disabled"
+  ll_addr                      = "::"
+  mac                          = "0F:0F:0F:0F:FF:FF"
+  mode                         = "native"
+  mtu                          = "inherit"
+  target_dscp                  = "AF11"
+
+}
+
+resource "aci_l3out_path_attachment_secondary_ip" "example" {
+
+  l3out_path_attachment_dn  = "${aci_l3out_path_attachment.example.id}"
+  addr  = "10.0.0.1/24"
+  annotation  = "example"
+  ipv6_dad = "disabled"
+  name_alias  = "example"
+
+}
---
+++ examples/static_node_mgmt_address/main.tf
@@ -0,0 +1,17 @@
+provider "aci" {
+  username = ""
+  password = ""
+  url      = ""
+  insecure = true
+}
+resource "aci_static_node_mgmt_address" "example" {
+  management_epg_dn = "${aci_node_mgmt_epg.example.id}"
+  t_dn              = "topology/pod-1/node-1"
+  type              = "out_of_band"
+  addr              = "10.20.30.40/20"
+  annotation        = "example"
+  description       = "from terraform"
+  gw                = "10.20.30.41"
+  v6_addr           = "1::40/64"
+  v6_gw             = "1::21"
+}
