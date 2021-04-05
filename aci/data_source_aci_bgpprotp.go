d47f94ac02fb4dc675c134eec6e5254261bc0f50
Ashutosh Soni <74891251+AshuSoni-crest@users.noreply.github.com>
2021-04-05 10:13:29 +0530

    CACIT-1814 added resource files of bgpProtP (#324)

---
+++ aci/data_source_aci_bgpprotp.go
@@ -0,0 +1,53 @@
+package aci
+
+import (
+	"fmt"
+	"github.com/ciscoecosystem/aci-go-client/client"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceAciL3outBGPProtocolProfile() *schema.Resource {
+	return &schema.Resource{
+
+		Read: dataSourceAciL3outBGPProtocolProfileRead,
+
+		SchemaVersion: 1,
+
+		Schema: AppendBaseAttrSchema(map[string]*schema.Schema{
+			"logical_node_profile_dn": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+
+			"annotation": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"name_alias": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		}),
+	}
+}
+
+func dataSourceAciL3outBGPProtocolProfileRead(d *schema.ResourceData, m interface{}) error {
+	aciClient := m.(*client.Client)
+
+	rn := fmt.Sprintf("protp")
+	LogicalNodeProfileDn := d.Get("logical_node_profile_dn").(string)
+
+	dn := fmt.Sprintf("%s/%s", LogicalNodeProfileDn, rn)
+
+	bgpProtP, err := getRemoteL3outBGPProtocolProfile(aciClient, dn)
+
+	if err != nil {
+		return err
+	}
+	d.SetId(dn)
+	setL3outBGPProtocolProfileAttributes(bgpProtP, d)
+	return nil
+}
