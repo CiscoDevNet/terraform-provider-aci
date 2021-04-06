7bf1dc73cacfc7fe2ddaceb4f259c1e597d46190
Ashutosh Soni <74891251+AshuSoni-crest@users.noreply.github.com>
2021-04-06 21:35:36 +0530

    CACIT-2130 added resource for l3extRouteTagPol (#327)

---
+++ aci/data_source_aci_l3extroutetagpol.go
@@ -0,0 +1,67 @@
+package aci
+
+import (
+	"fmt"
+
+	"github.com/ciscoecosystem/aci-go-client/client"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceAciL3outRouteTagPolicy() *schema.Resource {
+	return &schema.Resource{
+
+		Read: dataSourceAciL3outRouteTagPolicyRead,
+
+		SchemaVersion: 1,
+
+		Schema: AppendBaseAttrSchema(map[string]*schema.Schema{
+			"tenant_dn": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+
+			"annotation": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"name_alias": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"tag": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		}),
+	}
+}
+
+func dataSourceAciL3outRouteTagPolicyRead(d *schema.ResourceData, m interface{}) error {
+	aciClient := m.(*client.Client)
+
+	name := d.Get("name").(string)
+
+	rn := fmt.Sprintf("rttag-%s", name)
+	TenantDn := d.Get("tenant_dn").(string)
+
+	dn := fmt.Sprintf("%s/%s", TenantDn, rn)
+
+	l3extRouteTagPol, err := getRemoteL3outRouteTagPolicy(aciClient, dn)
+
+	if err != nil {
+		return err
+	}
+	d.SetId(dn)
+	setL3outRouteTagPolicyAttributes(l3extRouteTagPol, d)
+	return nil
+}
---
+++ vendor/github.com/ciscoecosystem/aci-go-client/client/l3extRouteTagPol_service.go
@@ -0,0 +1,53 @@
+package client
+
+import (
+	"fmt"
+
+	"github.com/ciscoecosystem/aci-go-client/models"
+)
+
+func (sm *ServiceManager) CreateL3outRouteTagPolicy(name string, tenant string, description string, l3extRouteTagPolattr models.L3outRouteTagPolicyAttributes) (*models.L3outRouteTagPolicy, error) {
+	rn := fmt.Sprintf("rttag-%s", name)
+	parentDn := fmt.Sprintf("uni/tn-%s", tenant)
+	l3extRouteTagPol := models.NewL3outRouteTagPolicy(rn, parentDn, description, l3extRouteTagPolattr)
+	err := sm.Save(l3extRouteTagPol)
+	return l3extRouteTagPol, err
+}
+
+func (sm *ServiceManager) ReadL3outRouteTagPolicy(name string, tenant string) (*models.L3outRouteTagPolicy, error) {
+	dn := fmt.Sprintf("uni/tn-%s/rttag-%s", tenant, name)
+	cont, err := sm.Get(dn)
+	if err != nil {
+		return nil, err
+	}
+
+	l3extRouteTagPol := models.L3outRouteTagPolicyFromContainer(cont)
+	return l3extRouteTagPol, nil
+}
+
+func (sm *ServiceManager) DeleteL3outRouteTagPolicy(name string, tenant string) error {
+	dn := fmt.Sprintf("uni/tn-%s/rttag-%s", tenant, name)
+	return sm.DeleteByDn(dn, models.L3extroutetagpolClassName)
+}
+
+func (sm *ServiceManager) UpdateL3outRouteTagPolicy(name string, tenant string, description string, l3extRouteTagPolattr models.L3outRouteTagPolicyAttributes) (*models.L3outRouteTagPolicy, error) {
+	rn := fmt.Sprintf("rttag-%s", name)
+	parentDn := fmt.Sprintf("uni/tn-%s", tenant)
+	l3extRouteTagPol := models.NewL3outRouteTagPolicy(rn, parentDn, description, l3extRouteTagPolattr)
+
+	l3extRouteTagPol.Status = "modified"
+	err := sm.Save(l3extRouteTagPol)
+	return l3extRouteTagPol, err
+
+}
+
+func (sm *ServiceManager) ListL3outRouteTagPolicy(tenant string) ([]*models.L3outRouteTagPolicy, error) {
+
+	baseurlStr := "/api/node/class"
+	dnUrl := fmt.Sprintf("%s/uni/tn-%s/l3extRouteTagPol.json", baseurlStr, tenant)
+
+	cont, err := sm.GetViaURL(dnUrl)
+	list := models.L3outRouteTagPolicyListFromContainer(cont)
+
+	return list, err
+}
