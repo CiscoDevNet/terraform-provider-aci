6acb42e3cad79dce054e0c76ec76bb805404a2a1
AshuSoni-crest <ashutosh.soni@crestdatasys.com>
2021-04-06 11:50:58 +0530

    CACIT-2130 added resource for l3extRouteTagPol

--- aci/provider.go
+++ aci/provider.go
@@ -171,6 +171,7 @@ func Provider() terraform.ResourceProvider {
 			"aci_l3out_hsrp_secondary_vip":                 resourceAciL3outHSRPSecondaryVIP(),
 			"aci_l3out_bfd_interface_profile":              resourceAciBFDInterfaceProfile(),
 			"aci_l3out_bgp_protocol_profile":               resourceAciL3outBGPProtocolProfile(),
+			"aci_l3out_route_tag_policy":                   resourceAciL3outRouteTagPolicy(),
 		},
 
 		DataSourcesMap: map[string]*schema.Resource{
@@ -291,6 +292,7 @@ func Provider() terraform.ResourceProvider {
 			"aci_l3out_hsrp_secondary_vip":                 dataSourceAciL3outHSRPSecondaryVIP(),
 			"aci_l3out_bfd_interface_profile":              dataSourceAciBFDInterfaceProfile(),
 			"aci_l3out_bgp_protocol_profile":               dataSourceAciL3outBGPProtocolProfile(),
+			"aci_l3out_route_tag_policy":                   dataSourceAciL3outRouteTagPolicy(),
 		},
 
 		ConfigureFunc: configureClient,
