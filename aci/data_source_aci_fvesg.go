c54dbc7f25cb96970b57038561832ff6e890fd88
ssrish <ssrish@cisco.com>
2021-04-22 23:45:04 -0700

    fvesg and fvesgselector via generation

--- aci/data_source_aci_fvesg.go
+++ aci/data_source_aci_fvesg.go
@@ -1,126 +1,66 @@
 package aci
 
-
 import (
 	"fmt"
+
 	"github.com/ciscoecosystem/aci-go-client/client"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
 func dataSourceAciEndpointSecurityGroup() *schema.Resource {
 	return &schema.Resource{
-
-		Read:   dataSourceAciEndpointSecurityGroupRead,
-
+		Read:          dataSourceAciEndpointSecurityGroupRead,
 		SchemaVersion: 1,
-
-		Schema: AppendBaseAttrSchema(map[string]*schema.Schema{
-			"application_profile_dn": &schema.Schema{
+		Schema: AppendBaseAttrSchema(AppendNameAliasAttrSchema(map[string]*schema.Schema{
+			"application_profile_dn": {
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			
-			
-			"name": &schema.Schema{
-				Type: schema.TypeString,
+			"annotation": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"flood_on_encap": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"match_t": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
 				Required: true,
 			},
-			
-			
-            
-			
-			"annotation": &schema.Schema{
+			"pc_enf_pref": {
 				Type:     schema.TypeString,
 				Optional: true,
 				Computed: true,
 			},
-			
-            
-			
-			"exception_tag": &schema.Schema{
+			"pref_gr_memb": {
 				Type:     schema.TypeString,
 				Optional: true,
 				Computed: true,
 			},
-			
-            
-			
-			"flood_on_encap": &schema.Schema{
+			"prio": {
 				Type:     schema.TypeString,
 				Optional: true,
 				Computed: true,
 			},
-			
-            
-			
-			"match_t": &schema.Schema{
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
-			},
-			
-            
-			
-			"name_alias": &schema.Schema{
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
-			},
-			
-            
-			
-			"pc_enf_pref": &schema.Schema{
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
-			},
-			
-            
-			
-			"pref_gr_memb": &schema.Schema{
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
-			},
-			
-            
-			
-			"prio": &schema.Schema{
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
-			},
-			
-            
-			
-			"userdom": &schema.Schema{
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
-			},
-			
-            
-            }),
-    }
+		})),
+	}
 }
-
-
 
 func dataSourceAciEndpointSecurityGroupRead(d *schema.ResourceData, m interface{}) error {
 	aciClient := m.(*client.Client)
-	
 	name := d.Get("name").(string)
-	
-
-    rn := fmt.Sprintf("esg-%s",name)
-    ApplicationProfileDn := d.Get("application_profile_dn").(string)
-
-    dn := fmt.Sprintf("%s/%s",ApplicationProfileDn,rn)
-    
-
-	
+	ApplicationProfileDn := d.Get("application_profile_dn").(string)
+	rn := fmt.Sprintf("esg-%s", name)
+	dn := fmt.Sprintf("%s/%s", ApplicationProfileDn, rn)
 	fvESg, err := getRemoteEndpointSecurityGroup(aciClient, dn)
-
 	if err != nil {
 		return err
 	}
