2680127fa67b59157915c66e9f2ad31352410e92
danischm <danischm@cisco.com>
2021-04-29 12:53:41 +0200

    Add additional tests and documentation

--- aci/resource_rest_managed_test.go
+++ aci/resource_rest_managed_test.go
@@ -12,35 +12,101 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
-func TestAccAciRestManaged_basic(t *testing.T) {
+func TestAccAciRestManaged_tenant(t *testing.T) {
 	name := acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckAciCDPInterfacePolicyDestroy,
+		CheckDestroy: testAccCheckAciRestManagedDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccAciRestManagedConfigTenant(name),
+				Config: testAccAciRestManagedConfigTenant(name, "Create description"),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckAciRestManagedCheckObject("aci_rest_managed.tenant"),
+					testAccCheckAciRestManagedCheckObject("aci_rest_managed.fvTenant"),
+				),
+			},
+			{
+				Config: testAccAciRestManagedConfigTenant(name, "Updated description"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAciRestManagedCheckObject("aci_rest_managed.fvTenant"),
 				),
 			},
 		},
 	})
 }
 
-func testAccAciRestManagedConfigTenant(name string) string {
+func TestAccAciRestManaged_connPref(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAciRestManagedStillExists,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAciRestManagedConfigConnPref("ooband"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAciRestManagedCheckObject("aci_rest_managed.mgmtConnectivityPrefs"),
+				),
+			},
+			{
+				Config: testAccAciRestManagedConfigConnPref("inband"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAciRestManagedCheckObject("aci_rest_managed.mgmtConnectivityPrefs"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAciRestManaged_noContent(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAciRestManagedStillExists,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAciRestManagedConfigConnPref(""),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAciRestManagedCheckObject("aci_rest_managed.mgmtConnectivityPrefs"),
+				),
+			},
+		},
+	})
+}
+
+func testAccAciRestManagedConfigTenant(name string, description string) string {
 	return fmt.Sprintf(`
-	resource "aci_rest_managed" "tenant" {
+	resource "aci_rest_managed" "fvTenant" {
 		dn = "uni/tn-%[1]s"
 		class_name = "fvTenant"
 		content = {
 			name = "%[1]s"
-			descr = "Test description"
+			descr = "%[2]s"
+			nameAlias = "Testacc_Tenant"
 		}
 	}
-	`, name)
+	`, name, description)
+}
+
+func testAccAciRestManagedConfigConnPref(status string) string {
+	if status != "" {
+		return fmt.Sprintf(`
+		resource "aci_rest_managed" "mgmtConnectivityPrefs" {
+			dn = "uni/fabric/connectivityPrefs"
+			class_name = "mgmtConnectivityPrefs"
+			content = {
+				interfacePref = "%[1]s"
+			}
+		}
+		`, status)
+	} else {
+		return `
+		resource "aci_rest_managed" "mgmtConnectivityPrefs" {
+			dn = "uni/fabric/connectivityPrefs"
+			class_name = "mgmtConnectivityPrefs"
+		}
+		`
+	}
 }
 
 func testAccCheckAciRestManagedCheckObject(name string) resource.TestCheckFunc {
@@ -92,6 +158,25 @@ func testAccCheckAciRestManagedDestroy(s *terraform.State) error {
 			_, err := client.Get(rs.Primary.ID)
 			if err == nil {
 				return fmt.Errorf("Resource aci_rest_managed %s still exists", rs.Primary.ID)
+			}
+
+		} else {
+			continue
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckAciRestManagedStillExists(s *terraform.State) error {
+	client := testAccProvider.Meta().(*client.Client)
+
+	for _, rs := range s.RootModule().Resources {
+
+		if rs.Type == "aci_rest_managed" {
+			_, err := client.Get(rs.Primary.ID)
+			if err != nil {
+				return fmt.Errorf("Error retrieving resource aci_rest_managed %s", rs.Primary.ID)
 			}
 
 		} else {
---
+++ website/docs/r/rest_managed.html.markdown
@@ -0,0 +1,46 @@
+---
+layout: "aci"
+page_title: "ACI: aci_rest_managed"
+sidebar_current: "docs-aci-resource-rest-managed"
+description: |-
+  Manages ACI Model Objects via REST API calls. Any Model Object that is not supported by provider can be created/managed using this resource. Compared to the aci_rest resource, this resource can only manage a single API object (no children), but is able to read the state and therefore reconcile configuration drift.
+---
+
+# aci_rest_managed #
+
+Manages ACI Model Objects via REST API calls. Any Model Object that is not supported by provider can be created/managed using this resource. Compared to the `aci_rest` resource, this resource can only manage a single API object (no children), but is able to read the state and therefore reconcile configuration drift.
+
+## Example Usage ##
+
+```hcl
+resource "aci_rest_managed" "fvTenant" {
+  dn         = "uni/tn-EXAMPLE_TENANT"
+  class_name = "fvTenant"
+  content = {
+    name  = "EXAMPLE_TENANT"
+    descr = "Example description"
+  }
+}
+
+resource "aci_rest_managed" "mgmtConnectivityPrefs" {
+  dn         = "uni/fabric/connectivityPrefs"
+  class_name = "mgmtConnectivityPrefs"
+  content = {
+    interfacePref = "ooband"
+  }
+}
+```
+
+## Argument Reference ##
+
+* `dn` - (Required) Distinguished name of object being managed including its relative name, e.g. uni/ten-EXAMPLE_TENANT.
+* `class_name` - (Required) Which class object is being created. (Make sure there is no colon in the classname)
+* `content` - (Optional) Map of key-value pairs those needed to be passed to the Model object as parameters. Make sure the key name matches the name with the object parameter in ACI.
+
+## Attribute Reference ##
+
+The only attribute that this resource exports is the `id`, which is set to the Dn of the object created by it.
+
+## Importing ##
+
+This resource does not support import.
