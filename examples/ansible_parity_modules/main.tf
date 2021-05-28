39f6f440a159a540de5f677779c8b21902dbb4c9
AshuSoni-crest <ashutosh.soni@crestdatasys.com>
2021-05-28 15:25:05 +0530

    removed credentials from example files.

--- examples/ansible_parity_modules/main.tf
+++ examples/ansible_parity_modules/main.tf
@@ -7,9 +7,9 @@ terraform {
 }
 
 provider "aci" {
-  username = "admin"
-  password = "ins3965!ins3965!"
-  url      = "https://173.36.219.26/"
+  username = ""
+  password = ""
+  url      = ""
   insecure = true
 }
 
