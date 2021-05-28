95fa3ccfa258dcd866ff4ef65b59aeed933c8fa7
Ashutosh Soni <74891251+AshuSoni-crest@users.noreply.github.com>
2021-05-28 13:58:54 +0530

    Example added. (#440)

--- examples/leaf_profile/l2.tf
+++ examples/leaf_profile/l2.tf
@@ -1,4 +1,25 @@
+terraform {
+  required_providers {
+    aci = {
+      source = "ciscodevnet/aci"
+    }
+  }
+}
+
+provider "aci" {
+  username = ""
+  password = ""
+  url      = ""
+  insecure = true
+}
 resource "aci_l2_interface_policy" "test_l2" {
-    name = "tf_l2"
+        name = "tf_l2"
+        description = "From Terraform"
+		name        = "demo_l2_pol"
+		annotation  = "tag_l2_pol"
+		name_alias  = "alias_l2_pol"
+		qinq        = "disabled"
+		vepa        = "disabled"
+		vlan_scope  = "global"
   
 }
--- examples/ospf_interface_policy/main.tf
+++ examples/ospf_interface_policy/main.tf
@@ -1,17 +1,28 @@
+terraform {
+  required_providers {
+    aci = {
+      source = "ciscodevnet/aci"
+    }
+  }
+}
+
 provider "aci" {
   username = ""
   password = ""
   url      = ""
   insecure = true
 }
-
+resource "aci_tenant" "dev_tenant" {
+  name        = "tf_test_rel_tenant"
+  description = "This tenant is created by terraform"
+}
 resource "aci_ospf_interface_policy" "fooospf_interface_policy" {
   tenant_dn    = aci_tenant.dev_tenant.id
-  description  = "%s"
+  description  = "From Terraform"
   name         = "demo_ospfpol"
   annotation   = "tag_ospf"
   cost         = "unspecified"
-  ctrl         = "%s"
+  ctrl         = "unspecified"
   dead_intvl   = "40"
   hello_intvl  = "10"
   name_alias   = "alias_ospf"
