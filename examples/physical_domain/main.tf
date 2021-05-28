1ed8cb781ff77fefc0f4d343410d332c2f6dbc24
AshuSoni-crest <ashutosh.soni@crestdatasys.com>
2021-05-28 14:46:22 +0530

    added examples for sprint 2 resources.

--- examples/physical_domain/main.tf
+++ examples/physical_domain/main.tf
@@ -14,11 +14,11 @@ provider "aci" {
 }
 
 resource "aci_vlan_pool" "PHYS-VLAN-POOL" {
-  name  = "PHYS-VLAN-POOL"
-  alloc_mode  = "static"
+  name       = "PHYS-VLAN-POOL"
+  alloc_mode = "static"
 }
 resource "aci_physical_domain" "PhyDom" {
-  depends_on = [aci_vlan_pool.PHYS-VLAN-POOL]
-  name  = "PhyDom"
+  depends_on                = [aci_vlan_pool.PHYS-VLAN-POOL]
+  name                      = "PhyDom"
   relation_infra_rs_vlan_ns = aci_vlan_pool.PHYS-VLAN-POOL.id
 }
