bea0d9e3b47ccb680393e9e13b016c22b2f8877c
AshuSoni-crest <ashutosh.soni@crestdatasys.com>
2021-06-14 15:30:51 +0530

    change description value in main.tf file of aci_l3out_loopback_interface_profile resource

--- examples/l3out_loopback_interface_profile/main.tf
+++ examples/l3out_loopback_interface_profile/main.tf
@@ -8,7 +8,7 @@ provider "aci" {
 resource "aci_l3out_loopback_interface_profile" "test" {
   fabric_node_dn = aci_logical_node_to_fabric_node.example.id
   addr           = "1.2.3.5"
-  description    = "%s"
-  annotation     = "example"
+  description    = "from terraform"
+  annotation     = "l3out_loopback_interface_profile_tag"
   name_alias     = "example"
 }
