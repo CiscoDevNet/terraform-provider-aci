af17f2c75f7b6ed2b88d939b3c3a16842bc467b2
Ashutosh Soni <74891251+AshuSoni-crest@users.noreply.github.com>
2021-06-15 17:17:52 +0530

    examples folder updated (#499)

---
+++ examples/aci_any/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aci = {
+      source = "ciscodevnet/aci"
+    }
+  }
+}
+
+provider "aci" {
+  username = ""
+  password = ""
+  url      = ""
+  insecure = true
+}
+
+resource "aci_tenant" "example" {
+		name 		= "tenant"
+		description = "tenant created while acceptance testing"
+
+	}
+resource "aci_vrf" "example" {
+		tenant_dn              = aci_tenant.example.id
+		name                   = "demo_vrf"
+}
+
+resource "aci_any" "example_vzany" {
+  vrf_dn       = aci_vrf.example.id
+  description  = "vzAny Description"
+  annotation   = "tag_any"
+  match_t      = "AtleastOne"
+  name_alias   = "alias_any"
+  pref_gr_memb = "disabled"
+}
