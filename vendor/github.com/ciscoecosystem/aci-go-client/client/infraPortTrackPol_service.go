a419ae26fd1822860dd46157ce67e4cb68821712
Rutvik Shiyani <88027613+RutvikS-crest@users.noreply.github.com>
2021-10-20 15:48:24 +0530

    added a new resource for aaaLdapGroupMap (#700)

--- vendor/github.com/ciscoecosystem/aci-go-client/client/infraPortTrackPol_service.go
+++ vendor/github.com/ciscoecosystem/aci-go-client/client/infraPortTrackPol_service.go
@@ -1,25 +1,20 @@
 package client
 
-
-
 import (
-	
 	"fmt"
 
 	"github.com/ciscoecosystem/aci-go-client/models"
-	
 )
 
-
-func (sm *ServiceManager) CreatePortTracking(name string, description string, nameAlias string, infraPortTrackPolAttr models.PortTrackingAttributes) (*models.PortTracking, error) {	
-	rn := fmt.Sprintf(models.RninfraPortTrackPol , name)
-	parentDn := fmt.Sprintf(models.ParentDninfraPortTrackPol, )
+func (sm *ServiceManager) CreatePortTracking(name string, description string, nameAlias string, infraPortTrackPolAttr models.PortTrackingAttributes) (*models.PortTracking, error) {
+	rn := fmt.Sprintf(models.RninfraPortTrackPol, name)
+	parentDn := fmt.Sprintf(models.ParentDninfraPortTrackPol)
 	infraPortTrackPol := models.NewPortTracking(rn, parentDn, description, nameAlias, infraPortTrackPolAttr)
 	err := sm.Save(infraPortTrackPol)
 	return infraPortTrackPol, err
 }
 
-func (sm *ServiceManager) ReadPortTracking(name string, ) (*models.PortTracking, error) {
+func (sm *ServiceManager) ReadPortTracking(name string) (*models.PortTracking, error) {
 	dn := fmt.Sprintf(models.DninfraPortTrackPol, name)
 	cont, err := sm.Get(dn)
 	if err != nil {
@@ -29,24 +24,23 @@ func (sm *ServiceManager) ReadPortTracking(name string, ) (*models.PortTracking,
 	return infraPortTrackPol, nil
 }
 
-func (sm *ServiceManager) DeletePortTracking(name string, ) error {
+func (sm *ServiceManager) DeletePortTracking(name string) error {
 	dn := fmt.Sprintf(models.DninfraPortTrackPol, name)
 	return sm.DeleteByDn(dn, models.InfraporttrackpolClassName)
 }
 
 func (sm *ServiceManager) UpdatePortTracking(name string, description string, nameAlias string, infraPortTrackPolAttr models.PortTrackingAttributes) (*models.PortTracking, error) {
-	rn := fmt.Sprintf(models.RninfraPortTrackPol , name)
-	parentDn := fmt.Sprintf(models.ParentDninfraPortTrackPol, )
+	rn := fmt.Sprintf(models.RninfraPortTrackPol, name)
+	parentDn := fmt.Sprintf(models.ParentDninfraPortTrackPol)
 	infraPortTrackPol := models.NewPortTracking(rn, parentDn, description, nameAlias, infraPortTrackPolAttr)
-    infraPortTrackPol.Status = "modified"
+	infraPortTrackPol.Status = "modified"
 	err := sm.Save(infraPortTrackPol)
 	return infraPortTrackPol, err
 }
 
-func (sm *ServiceManager) ListPortTracking() ([]*models.PortTracking, error) {	
+func (sm *ServiceManager) ListPortTracking() ([]*models.PortTracking, error) {
 	dnUrl := fmt.Sprintf("%s/uni/infra/infraPortTrackPol.json", models.BaseurlStr)
-    cont, err := sm.GetViaURL(dnUrl)
+	cont, err := sm.GetViaURL(dnUrl)
 	list := models.PortTrackingListFromContainer(cont)
 	return list, err
 }
-
