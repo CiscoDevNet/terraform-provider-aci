cf8d73915be4d0018abfa8517ba0143e583186dd
Shreyas <ssrish@cisco.com>
2021-11-01 09:22:09 -0700

    Addition of example for tag selector

---
+++ examples/aci_esg_tag_selector/aci_esg_tag_selector.tf
@@ -0,0 +1,27 @@
+resource "aci_tenant" "terraform_tenant" {
+  name        = "tf_tenant"
+  description = "This tenant is created by terraform"
+}
+
+resource "aci_application_profile" "terraform_ap" {
+  tenant_dn = aci_tenant.terraform_tenant.id
+  name      = "tf_ap"
+}
+
+# create ESG
+resource "aci_endpoint_security_group" "terraform_esg" {
+  application_profile_dn = aci_application_profile.terraform_ap.id
+  name                   = "tf_esg"
+}
+
+# create selector
+resource "aci_endpoint_security_group_tag_selector" "terraform_tag_selector" {
+    endpoint_security_group_dn  = aci_endpoint_security_group.terraform_esg.id
+    match_key                    = "example"
+    match_value                  = "example1"
+    value_operator               = "regex"
+    description                  = "Tag Selector"
+    
+}
+
+
