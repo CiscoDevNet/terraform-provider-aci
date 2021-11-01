16a639fc2a97013295b2202d50eea30ba8ab8149
RutvikS-crest <rutvik.shiyani@crestdatasys.com>
2021-11-01 08:24:09 +0530

    removed .vscode

--- .vscode/move_binary.sh
+++
@@ -1,5 +0,0 @@
-mv $GOPATH/bin/$1 $APPDATA/terraform.d/plugins/github.com/CiscoDevNet/aci/1.4.0/windows_amd64/
-cd $2
-rm -rf .terraform*
-echo "removed terraform build successfully"
-terraform init
