9e881b12279e7a27d04e0bcd8247925c0128f8c4
Shreyas <ssrish@cisco.com>
2022-01-25 08:17:51 -0800

    fix ep_move_detect issue

--- aci/resource_aci_fvbd.go
+++ aci/resource_aci_fvbd.go
@@ -1065,14 +1065,15 @@ func resourceAciBridgeDomainRead(ctx context.Context, d *schema.ResourceData, m 
 	dn := d.Id()
 	fvBD, err := getRemoteBridgeDomain(aciClient, dn)
 
-	if fvBD.EpMoveDetectMode == "" {
-		fvBD.EpMoveDetectMode = "disable"
-	}
-
 	if err != nil {
 		d.SetId("")
 		return nil
 	}
+
+	if fvBD.EpMoveDetectMode == "" {
+		fvBD.EpMoveDetectMode = "disable"
+	}
+
 	_, err = setBridgeDomainAttributes(fvBD, d)
 
 	if err != nil {
