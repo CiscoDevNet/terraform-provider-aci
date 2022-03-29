6e2806de097f711dd66d7c43807593e1ce03e517
cindy zhao <xinyuezhao96@gmail.com>
2022-03-29 09:16:50 -0700

    [bugfix] update example for i3out_bgp_protocol_profile

--- examples/l3out_bgp_protocol_profile/main.tf
+++ examples/l3out_bgp_protocol_profile/main.tf
@@ -14,8 +14,48 @@ provider "aci" {
   insecure = true
 }
 
-resource "aci_l3out_bgp_protocol_profile" "example" {
-  logical_node_profile_dn = aci_logical_node_profile.example.id
-  annotation              = "example"
-  name_alias              = "example"
+resource "aci_tenant" "footenant" {
+	name 		= "tenant_bgp_protp"
+	description = "tenant created while acceptance testing"
+}
+
+resource "aci_l3_outside" "fool3_outside" {
+  name 		= "l3_bgp_protp"
+  description = "l3_outside created while acceptance testing"
+  tenant_dn = aci_tenant.footenant.id
+}
+
+resource "aci_logical_node_profile" "foological_node_profile" {
+  name 		= "logical_node_profile"
+  description = "logical_node_profile created while acceptance testing"
+  l3_outside_dn = aci_l3_outside.fool3_outside.id
+}
+
+resource "aci_bgp_best_path_policy" "foobgp_best_path_policy" {
+  tenant_dn  = aci_tenant.footenant.id
+  name       = "rs_bgp_best_path_policy"
+  annotation = "best_path_example"
+  ctrl       = "asPathMultipathRelax"
+  name_alias = "best_path_example"
+}
+
+resource "aci_bgp_timers" "foobgp_timer" {
+  tenant_dn    = aci_tenant.footenant.id
+  name         = "one"
+  annotation   = "bgp_timer_example"
+  gr_ctrl      = "helper"
+  hold_intvl   = "189"
+  ka_intvl     = "65"
+  max_as_limit = "70"
+  name_alias   = "bgp_timer_aliasing"
+  stale_intvl  = "15"
+}
+
+resource "aci_l3out_bgp_protocol_profile" "foolbgp_protp" {
+  logical_node_profile_dn = aci_logical_node_profile.foological_node_profile.id
+  annotation              = "bgp_protp_annotation"
+  name_alias              = "bgp_protp_name_alias"
+  name = "bgp_protp_test"
+  relation_bgp_rs_best_path_ctrl_pol = aci_bgp_best_path_policy.foobgp_best_path_policy.id
+  relation_bgp_rs_bgp_node_ctx_pol = aci_bgp_timers.foobgp_timer.id
 }
