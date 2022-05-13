5a31cc78a0e9da77318b4c659edefadfac051f52
Shreyas <ssrish@cisco.com>
2022-05-12 17:58:01 -0700

    [ignore] Update vendor files v1.38.0

--- aci/resource_aci_vnscdev_test.go
+++ aci/resource_aci_vnscdev_test.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/ciscoecosystem/aci-go-client/client"
 	"github.com/ciscoecosystem/aci-go-client/models"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
