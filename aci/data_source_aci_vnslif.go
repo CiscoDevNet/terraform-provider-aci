22c91f137706274231eac948aa59cabb75076166
Shreyas <ssrish@cisco.com>
2022-05-26 15:02:26 -0700

    [minor_change] Addition of new resource and data source for L4-L7 logical interfaces (vnsLIf) (#865)

---
+++ aci/data_source_aci_vnslif.go
@@ -0,0 +1,78 @@
+package aci
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sort"
+
+	"github.com/ciscoecosystem/aci-go-client/client"
+	"github.com/ciscoecosystem/aci-go-client/models"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceAciLogicalInterface() *schema.Resource {
+	return &schema.Resource{
+		ReadContext:   dataSourceAciLogicalInterfaceRead,
+		SchemaVersion: 1,
+		Schema: AppendBaseAttrSchema(AppendNameAliasAttrSchema(map[string]*schema.Schema{
+			"l4_l7_device_dn": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"encap": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"enhanced_lag_policy_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"relation_vns_rs_c_if_att_n": {
+				Type:        schema.TypeSet,
+				Elem:        &schema.Schema{Type: schema.TypeString},
+				Optional:    true,
+				Description: "Create relation to vnsCIf",
+				Set:         schema.HashString,
+			}})),
+	}
+}
+
+func dataSourceAciLogicalInterfaceRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	aciClient := m.(*client.Client)
+	name := d.Get("name").(string)
+	L4_L7DevicesDn := d.Get("l4_l7_device_dn").(string)
+	rn := fmt.Sprintf(models.RnvnsLIf, name)
+	dn := fmt.Sprintf("%s/%s", L4_L7DevicesDn, rn)
+
+	vnsLIf, err := getRemoteLogicalInterface(aciClient, dn)
+	if err != nil {
+		return nil
+	}
+
+	d.SetId(dn)
+
+	_, err = setLogicalInterfaceAttributes(vnsLIf, d)
+	if err != nil {
+		return nil
+	}
+
+	vnsRsCIfAttNData, err := aciClient.ReadRelationvnsRsCIfAttN(dn)
+	if err != nil {
+		log.Printf("[DEBUG] Error while reading relation vnsRsCIfAttN %v", err)
+		d.Set("relation_vns_rs_c_if_att_n", make([]string, 0, 1))
+	} else {
+		vnsRsCIfAttNDataList := toStringList(vnsRsCIfAttNData.(*schema.Set).List())
+		sort.Strings(vnsRsCIfAttNDataList)
+		d.Set("relation_vns_rs_c_if_att_n", vnsRsCIfAttNDataList)
+
+	}
+	return nil
+}
---
+++ aci/resource_aci_vnslif_test.go
@@ -0,0 +1,148 @@
+package aci
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ciscoecosystem/aci-go-client/client"
+	"github.com/ciscoecosystem/aci-go-client/models"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccAciLogicalInterface_Basic(t *testing.T) {
+	var logical_interface models.LogicalInterface
+	fv_tenant_name := acctest.RandString(5)
+	vns_l_dev_vip_name := acctest.RandString(5)
+	vns_l_if_name := acctest.RandString(5)
+	description := "logical_interface created while acceptance testing"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAciLogicalInterfaceDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAciLogicalInterfaceConfig_basic(fv_tenant_name, vns_l_dev_vip_name, vns_l_if_name),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAciLogicalInterfaceExists("aci_logical_interface.foo_logical_interface", &logical_interface),
+					testAccCheckAciLogicalInterfaceAttributes(fv_tenant_name, vns_l_dev_vip_name, vns_l_if_name, description, &logical_interface),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAciLogicalInterface_Update(t *testing.T) {
+	var logical_interface models.LogicalInterface
+	fv_tenant_name := acctest.RandString(5)
+	vns_l_dev_vip_name := acctest.RandString(5)
+	vns_l_if_name := acctest.RandString(5)
+	description := "logical_interface created while acceptance testing"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAciLogicalInterfaceDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAciLogicalInterfaceConfig_basic(fv_tenant_name, vns_l_dev_vip_name, vns_l_if_name),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAciLogicalInterfaceExists("aci_logical_interface.foo_logical_interface", &logical_interface),
+					testAccCheckAciLogicalInterfaceAttributes(fv_tenant_name, vns_l_dev_vip_name, vns_l_if_name, description, &logical_interface),
+				),
+			},
+			{
+				Config: testAccCheckAciLogicalInterfaceConfig_basic(fv_tenant_name, vns_l_dev_vip_name, vns_l_if_name),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAciLogicalInterfaceExists("aci_logical_interface.foo_logical_interface", &logical_interface),
+					testAccCheckAciLogicalInterfaceAttributes(fv_tenant_name, vns_l_dev_vip_name, vns_l_if_name, description, &logical_interface),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckAciLogicalInterfaceConfig_basic(fv_tenant_name, vns_l_dev_vip_name, vns_l_if_name string) string {
+	return fmt.Sprintf(`
+
+	resource "aci_tenant" "foo_tenant" {
+		name 		= "%s"
+		description = "tenant created while acceptance testing"
+
+	}
+
+	resource "aci_l4-l7_devices" "foo_l4-l7_devices" {
+		name 		= "%s"
+		description = "l4-l7_devices created while acceptance testing"
+		tenant_dn = aci_tenant.foo_tenant.id
+	}
+
+	resource "aci_logical_interface" "foo_logical_interface" {
+		name 		= "%s"
+		description = "logical_interface created while acceptance testing"
+		l4-l7_devices_dn = aci_l4-l7_devices.foo_l4-l7_devices.id
+	}
+
+	`, fv_tenant_name, vns_l_dev_vip_name, vns_l_if_name)
+}
+
+func testAccCheckAciLogicalInterfaceExists(name string, logical_interface *models.LogicalInterface) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[name]
+
+		if !ok {
+			return fmt.Errorf("Logical Interface %s not found", name)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No Logical Interface dn was set")
+		}
+
+		client := testAccProvider.Meta().(*client.Client)
+
+		cont, err := client.Get(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		logical_interfaceFound := models.LogicalInterfaceFromContainer(cont)
+		if logical_interfaceFound.DistinguishedName != rs.Primary.ID {
+			return fmt.Errorf("Logical Interface %s not found", rs.Primary.ID)
+		}
+		*logical_interface = *logical_interfaceFound
+		return nil
+	}
+}
+
+func testAccCheckAciLogicalInterfaceDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*client.Client)
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type == "aci_logical_interface" {
+			cont, err := client.Get(rs.Primary.ID)
+			logical_interface := models.LogicalInterfaceFromContainer(cont)
+			if err == nil {
+				return fmt.Errorf("Logical Interface %s Still exists", logical_interface.DistinguishedName)
+			}
+		} else {
+			continue
+		}
+	}
+	return nil
+}
+
+func testAccCheckAciLogicalInterfaceAttributes(fv_tenant_name, vns_l_dev_vip_name, vns_l_if_name, description string, logical_interface *models.LogicalInterface) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		if vns_l_if_name != GetMOName(logical_interface.DistinguishedName) {
+			return fmt.Errorf("Bad vnsl_if %s", GetMOName(logical_interface.DistinguishedName))
+		}
+
+		if vns_l_dev_vip_name != GetMOName(GetParentDn(logical_interface.DistinguishedName, logical_interface.Rn)) {
+			return fmt.Errorf(" Bad vnsl_dev_vip %s", GetMOName(GetParentDn(logical_interface.DistinguishedName, logical_interface.Rn)))
+		}
+		if description != logical_interface.Description {
+			return fmt.Errorf("Bad logical_interface Description %s", logical_interface.Description)
+		}
+		return nil
+	}
+}
