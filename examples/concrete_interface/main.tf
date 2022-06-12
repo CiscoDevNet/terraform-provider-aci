59bab57cf5500c1f8efc41578550cf9bb0fa1c68
Shreyas <ssrish@cisco.com>
2022-06-11 17:51:40 -0700

    [minor_change] Add data source aci_l4_l7_deployed_graph_connector_vlan

--- examples/concrete_interface/main.tf
+++ examples/concrete_interface/main.tf
@@ -19,17 +19,17 @@ resource "aci_tenant" "terraform_tenant" {
 }
 
 resource "aci_l4_l7_device" "virtual_device" {
-  tenant_dn     = aci_tenant.terraform_tenant.id
-  name          = "tenant1-ASAv"
-  active        = "no"
-  context_aware = "single-Context"
-  devtype       = "VIRTUAL"
-  func_type     = "GoTo"
-  is_copy       = "no"
-  mode          = "legacy-Mode"
-  prom_mode     = "no"
-  svc_type      = "FW"
-  trunking      = "no"
+  tenant_dn        = aci_tenant.terraform_tenant.id
+  name             = "tenant1-ASAv"
+  active           = "no"
+  context_aware    = "single-Context"
+  device_type      = "VIRTUAL"
+  function_type    = "GoTo"
+  is_copy          = "no"
+  mode             = "legacy-Mode"
+  promiscuous_mode = "no"
+  service_type     = "FW"
+  trunking         = "no"
   relation_vns_rs_al_dev_to_dom_p {
     domain_dn = "uni/vmmp-VMware/dom-ACI-vDS"
   }
@@ -70,8 +70,8 @@ resource "aci_l4_l7_device" "physical_device" {
 }
 
 resource "aci_concrete_device" "physical_concrete" {
-  l4_l7_device_dn   = aci_l4_l7_device.physical_device.id
-  name              = "physical-Device"
+  l4_l7_device_dn = aci_l4_l7_device.physical_device.id
+  name            = "physical-Device"
 }
 
 # Creating an interface for a Physical Concrete Device
--- examples/l4_l7_concrete_device/main.tf
+++ examples/l4_l7_concrete_device/main.tf
@@ -19,17 +19,17 @@ resource "aci_tenant" "terraform_tenant" {
 }
 
 resource "aci_l4_l7_device" "virtual" {
-  tenant_dn     = aci_tenant.terraform_tenant.id
-  name          = "tenant1-ASAv"
-  active        = "no"
-  context_aware = "single-Context"
-  devtype       = "VIRTUAL"
-  func_type     = "GoTo"
-  is_copy       = "no"
-  mode          = "legacy-Mode"
-  prom_mode     = "no"
-  svc_type      = "FW"
-  trunking      = "no"
+  tenant_dn        = aci_tenant.terraform_tenant.id
+  name             = "tenant1-ASAv"
+  active           = "no"
+  context_aware    = "single-Context"
+  device_type      = "VIRTUAL"
+  function_type    = "GoTo"
+  is_copy          = "no"
+  mode             = "legacy-Mode"
+  promiscuous_mode = "no"
+  service_type     = "FW"
+  trunking         = "no"
   relation_vns_rs_al_dev_to_dom_p {
     domain_dn = "uni/vmmp-VMware/dom-ACI-vDS"
   }
@@ -57,6 +57,6 @@ resource "aci_l4_l7_device" "physical" {
 }
 
 resource "aci_concrete_device" "example2" {
-  l4_l7_device_dn   = aci_l4_l7_device.physical.id
-  name              = "physical-Device"
+  l4_l7_device_dn = aci_l4_l7_device.physical.id
+  name            = "physical-Device"
 }
