e983d7dc5dbe2b9438aabfb3a86357279bea21f9
Sabari Jaganathan <93724860+sajagana@users.noreply.github.com>
2022-07-30 00:06:36 +0530

    [minor_change] Add aci_vrf_leak_epg_bd_subnet resource and data source (leakRoutes, leakInternalSubnet and leakTo) (#900)

---
+++ aci/data_source_aci_leakinternalsubnet.go
@@ -0,0 +1,82 @@
+package aci
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/ciscoecosystem/aci-go-client/client"
+	"github.com/ciscoecosystem/aci-go-client/models"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceAciLeakInternalSubnet() *schema.Resource {
+	return &schema.Resource{
+		ReadContext:   dataSourceAciLeakInternalSubnetRead,
+		SchemaVersion: 1,
+		Schema: AppendBaseAttrSchema(AppendNameAliasAttrSchema(map[string]*schema.Schema{
+			"vrf_dn": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"ip": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			// True -> public, False -> private, Default -> false(private)
+			"allow_l3out_advertisement": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			// Tenant and VRF Destinations
+			"leak_to": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"vrf_dn": &schema.Schema{
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						// True -> public, False -> private, Default -> "inherit"
+						"allow_l3out_advertisement": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+					},
+				},
+			},
+		})),
+	}
+}
+
+func dataSourceAciLeakInternalSubnetRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	log.Printf("[DEBUG] LeakInternalSubnet: Beginning of data source read")
+	aciClient := m.(*client.Client)
+	ip := d.Get("ip").(string)
+	VRFDn := d.Get("vrf_dn").(string)
+	rn := fmt.Sprintf(models.RnleakInternalSubnet, ip)
+	dn := fmt.Sprintf("%s/%s", VRFDn, rn)
+
+	leakInternalSubnet, err := getRemoteLeakInternalSubnet(aciClient, dn)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(dn)
+
+	_, err = setLeakInternalSubnetAttributes(leakInternalSubnet, d)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	// leakTo - Beginning Read
+	_, err = getAndSetLeakToObjects(aciClient, dn, d)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	// leakTo - Read finished successfully
+	log.Printf("[DEBUG] LeakInternalSubnet: %s data source read finished successfully", d.Id())
+	return nil
+}
---
+++ examples/vrf_leak_epg_bd_subnet/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    aci = {
+      source = "ciscodevnet/aci"
+    }
+  }
+}
+
+provider "aci" {
+  username = "" # <APIC username>
+  password = "" # <APIC pwd>
+  url      = "" # <cloud APIC URL>
+  insecure = true
+}
+
+data "aci_tenant" "common" {
+  name = "common"
+}
+
+data "aci_vrf" "default" {
+  tenant_dn = data.aci_tenant.common.id
+  name      = "default"
+}
+
+resource "aci_tenant" "terraform_vrf" {
+  name = "terraform_vrf"
+}
+
+resource "aci_vrf" "vrf1" {
+  tenant_dn = aci_tenant.terraform_vrf.id
+  name      = "vrf1"
+}
+
+resource "aci_vrf" "vrf2" {
+  tenant_dn = aci_tenant.terraform_vrf.id
+  name      = "vrf2"
+}
+
+resource "aci_vrf_leak_epg_bd_subnet" "vrf_leak_epg_bd_subnet" {
+  vrf_dn                    = aci_vrf.vrf1.id
+  ip                        = "1.1.20.2/24"
+  allow_l3out_advertisement = true
+  leak_to {
+    vrf_dn                    = data.aci_vrf.default.id
+    allow_l3out_advertisement = "inherit"
+  }
+  leak_to {
+    vrf_dn                    = aci_vrf.vrf2.id
+    allow_l3out_advertisement = true
+  }
+}
