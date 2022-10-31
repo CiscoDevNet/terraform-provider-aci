ff0bffd8bfdc5bf4fe661d38525110adbcf31649
RutvikS-crest <rutvik.shiyani@crestdatasys.com>
2022-10-31 13:01:02 +0530

    added Annotation Schema function

--- aci/base_attr_schema.go
+++ aci/base_attr_schema.go
@@ -2,24 +2,6 @@ package aci
 
 import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
-func GetBaseAttrSchema() map[string]*schema.Schema {
-	return map[string]*schema.Schema{
-		"description": &schema.Schema{
-			Type:     schema.TypeString,
-			Optional: true,
-			Computed: true,
-		},
-		"annotation": &schema.Schema{
-			Type:     schema.TypeString,
-			Optional: true,
-			Computed: true,
-			DefaultFunc: func() (interface{}, error) {
-				return "orchestrator:terraform", nil
-			},
-		},
-	}
-}
-
 func GetAnnotationAttrSchema() map[string]*schema.Schema {
 	return map[string]*schema.Schema{
 		"annotation": {
@@ -45,12 +27,19 @@ func GetDescriptionAttrSchema() map[string]*schema.Schema {
 
 func GetAllowEmptyAttrSchema() map[string]*schema.Schema {
 	return map[string]*schema.Schema{
-		"allow_empty_result": &schema.Schema{
+		"allow_empty_result": {
 			Type:     schema.TypeBool,
 			Optional: true,
 			Default:  false,
 		},
 	}
+}
+
+func GetBaseAttrSchema() map[string]*schema.Schema {
+	return AppendAttrSchemas(
+		GetAnnotationAttrSchema(),
+		GetDescriptionAttrSchema(),
+	)
 }
 
 // AppendBaseAttrSchema adds the BaseAttr to any schema
