5344425c4a7a9dc28a97c2a808a3e1fb2413eb61
Shreyas <ssrish@cisco.com>
2023-07-10 18:16:56 -0400

    [ignore_changes] Included the zone mapped to default in the examples

--- examples/aci_cloud_template_region/main.tf
+++ examples/aci_cloud_template_region/main.tf
@@ -24,6 +24,12 @@ data "aci_cloud_context_profile" "hub_vnet" {
   name       = "ct_ctxprofile_westus"
 }
 
+# Secondary VRF to host new CIDRs within Hub VNet.
+resource "aci_vrf" "services_vrf" {
+  tenant_dn = "uni/tn-infra" 
+  name      = "services_vrf"
+}
+
 resource "aci_cloud_cidr_pool" "azure_cloud_cidr_pool" {
   cloud_context_profile_dn = data.aci_cloud_context_profile.hub_vnet.id
   addr                     = "10.8.0.0/25"
@@ -34,5 +40,6 @@ resource "aci_cloud_subnet" "azure_subnets" {
   cloud_cidr_pool_dn              = aci_cloud_cidr_pool.azure_cloud_cidr_pool.id
   name                            = "test"
   ip                              = "10.8.0.3/25"
-  relation_cloud_rs_subnet_to_ctx = "uni/tn-infra/ctx-ave-ctrl"
+  relation_cloud_rs_subnet_to_ctx = aci_vrf.services_vrf.id
+  zone                            = "uni/clouddomp/provp-azure/region-westus/zone-default"
 }
