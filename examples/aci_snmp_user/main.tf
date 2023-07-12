b7de6811671ddf2651ef9fe9ec0e5811d324949a
Abraham Mughal <abrahamu@umich.edu>
2023-07-12 15:48:55 -0500

    [ignore]

---
+++ examples/aci_snmp_user/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aci = {
+      source = "ciscodevnet/aci"
+    }
+  }
+}
+
+provider "aci" {
+  username = ""
+  password = ""
+  url      = ""
+  insecure = true
+}
+
+resource "aci_snmp_user" "foo_snmp_user" {
+    snmp_policy_dn = "uni/fabric/snmppol-default"
+    name     = "Greg"
+    authorization_key = "testing123"
+    authorization_type = "hmac-md5-96"
+    privacy_key = "my_privacy_key"
+    privacy_type = "aes-128"
+}
+
+resource "aci_snmp_user" "foo_snmp_user_1" {
+    snmp_policy_dn = "uni/fabric/snmppol-default"
+    name     = "George"
+    authorization_key = "testing123"
+    authorization_type = "hmac-md5-96"
+    privacy_key = null
+    privacy_type = null
+}
