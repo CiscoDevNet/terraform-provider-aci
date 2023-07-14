06a68c55edfc626d7b92c84fd0500ee618341c19
Gaspard Micol <129436116+gmicol@users.noreply.github.com>
2023-07-14 14:19:11 -0400

    [ignore] Add VRF, BD from "common" tenant in aci_application_epg example (#1067)

--- examples/application_epg/main.tf
+++ examples/application_epg/main.tf
@@ -1,9 +1,20 @@
+terraform {
+  required_providers {
+    aci = {
+      source = "ciscodevnet/aci"
+    }
+  }
+}
 
 provider "aci" {
   username = ""
   password = ""
   url      = ""
   insecure = true
+}
+
+resource "aci_tenant" "dev_tenant" {
+  name = "demo_tenant"
 }
 
 resource "aci_application_profile" "test_ap" {
@@ -31,4 +42,29 @@ resource "aci_application_epg" "fooapplication_epg" {
   pref_gr_memb           = "exclude"
   prio                   = "unspecified"
   shutdown               = "no"
+}
+
+/*
+The following depicts an example to create and associate an application EPG with the common Tenant's BD and VRF
+*/
+
+data "aci_tenant" "common_tenant" {
+  name = "common"
+}
+
+data "aci_vrf" "default_vrf" {
+  tenant_dn = data.aci_tenant.common_tenant.id
+  name      = "default"
+}
+
+resource "aci_bridge_domain" "test_bd" {
+  tenant_dn          = data.aci_tenant.common_tenant.id
+  name               = "common_test_bd"
+  relation_fv_rs_ctx = data.aci_vrf.default_vrf.id
+}
+
+resource "aci_application_epg" "test_epg_common" {
+  application_profile_dn = aci_application_profile.test_ap.id
+  name                   = "common_test_epg"
+  relation_fv_rs_bd      = aci_bridge_domain.test_bd.id
 }
