c5a1e88b136f73afd99708fd154921358115f7f8
Anvitha-Jain <anvithjain@gmail.com>
2023-07-27 13:16:01 -0700

    [minor_changes] Add new aci_bfd_multihop_interface_policy and aci_bfd_multihop_interface_profile resources  for BFD Multihop Interface policy and profile. (#1066)

---
+++ aci/data_source_aci_bfdmhifpol.go
@@ -0,0 +1,66 @@
+package aci
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ciscoecosystem/aci-go-client/v2/client"
+	"github.com/ciscoecosystem/aci-go-client/v2/models"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceAciBfdMultihopInterfacePolicy() *schema.Resource {
+	return &schema.Resource{
+		ReadContext:   dataSourceAciBfdMultihopInterfacePolicyRead,
+		SchemaVersion: 1,
+		Schema: AppendBaseAttrSchema(AppendNameAliasAttrSchema(map[string]*schema.Schema{
+			"tenant_dn": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"admin_state": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"detection_multiplier": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"min_receive_interval": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"min_transmit_interval": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		})),
+	}
+}
+
+func dataSourceAciBfdMultihopInterfacePolicyRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	aciClient := m.(*client.Client)
+	name := d.Get("name").(string)
+	tenantDn := d.Get("tenant_dn").(string)
+	rn := fmt.Sprintf(models.RnbfdMhIfPol, name)
+	dn := fmt.Sprintf("%s/%s", tenantDn, rn)
+
+	bfdMhIfPol, err := getAciBfdMultihopInterfacePolicy(aciClient, dn)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(dn)
+
+	_, err = setAciBfdMultihopInterfacePolicyAttributes(bfdMhIfPol, d)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/ciscoecosystem/aci-go-client/v2 v2.21.0
 	github.com/ghodss/yaml v1.0.0
+	github.com/hashicorp/go-cty v1.4.1-0.20200414143053-d3edf31b6320
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.25.0
 )
 
@@ -17,7 +18,6 @@ require (
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-checkpoint v0.5.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
-	github.com/hashicorp/go-cty v1.4.1-0.20200414143053-d3edf31b6320 // indirect
 	github.com/hashicorp/go-hclog v1.4.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/go-plugin v1.4.8 // indirect
