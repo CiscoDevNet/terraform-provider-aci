06fb0f70724597cf961503738aff9eee4c671ada
Lionel Hercot <lhercot@cisco.com>
2023-08-04 10:24:27 -0700

    [ignore] Small syntax fix to fix build for v2.10.1 release

--- aci/resource_aci_configexportp.go
+++ aci/resource_aci_configexportp.go
@@ -94,8 +94,8 @@ func resourceAciConfigurationExportPolicy() *schema.Resource {
 			"relation_config_rs_export_destination": &schema.Schema{
 				Type: schema.TypeString,
 
-				Optional: true,
-				Deprecated: "use relation_config_rs_remote_path instead"
+				Optional:   true,
+				Deprecated: "use relation_config_rs_remote_path instead",
 			},
 			"relation_trig_rs_triggerable": &schema.Schema{
 				Type: schema.TypeString,
