201ce0a853faeaf776f33ddb70f1da36578b3e1d
Sabari Jaganathan <93724860+sajagana@users.noreply.github.com>
2023-10-03 07:37:22 +0530

    [ignore] Appended GetAnnotationAttrSchema function to GetBaseAttrSchema in the base_attr_schema.go

--- aci/base_attr_schema.go
+++ aci/base_attr_schema.go
@@ -3,21 +3,13 @@ package aci
 import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
 func GetBaseAttrSchema() map[string]*schema.Schema {
-	return map[string]*schema.Schema{
+	return AppendAttrSchemas(map[string]*schema.Schema{
 		"description": &schema.Schema{
 			Type:     schema.TypeString,
 			Optional: true,
 			Computed: true,
 		},
-		"annotation": &schema.Schema{
-			Type:     schema.TypeString,
-			Optional: true,
-			Computed: true,
-			DefaultFunc: func() (interface{}, error) {
-				return "orchestrator:terraform", nil
-			},
-		},
-	}
+	}, GetAnnotationAttrSchema())
 }
 
 func GetAllowEmptyAttrSchema() map[string]*schema.Schema {
