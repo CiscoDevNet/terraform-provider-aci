2b6ec13fe6026d0ced727cd92183df33e0ca41a1
akinross <akinross@cisco.com>
2023-10-12 09:17:27 +0200

    [ignore] fix formtting of filter resource and examples

--- examples/filter/filter.tf
+++ examples/filter/filter.tf
@@ -4,8 +4,8 @@ resource "aci_tenant" "tenant_for_filter" {
 }
 
 resource "aci_filter" "demo_filter" {
-  tenant_dn                      = aci_tenant.tenant_for_filter.id
-  name                           = "test_tf_filter"
-  description                    = "This filter is created by terraform ACI provider."
-  relation_vz_rs_filt_graph_att  = "testinterm"  # Relation to vnsInTerm class.
+  tenant_dn                     = aci_tenant.tenant_for_filter.id
+  name                          = "test_tf_filter"
+  description                   = "This filter is created by terraform ACI provider."
+  relation_vz_rs_filt_graph_att = "testinterm" # Relation to vnsInTerm class.
 }
