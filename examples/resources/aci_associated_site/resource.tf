73d10c2cf971c6fb0c94a2f42dacfdd38c3c3ee3
Abraham Mughal <abmughal@cisco.com>
2024-06-04 16:40:07 -0700

    [minor_change]Fixed attribute and doc issues

--- examples/resources/aci_associated_site/resource.tf
+++ examples/resources/aci_associated_site/resource.tf
@@ -1,8 +1,10 @@
 
 resource "aci_associated_site" "example_application_epg" {
   parent_dn = aci_application_epg.example.id
+  site_id   = "0"
 }
 
 resource "aci_associated_site" "example_vrf" {
   parent_dn = aci_vrf.example.id
+  site_id   = "0"
 }
