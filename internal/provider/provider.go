b10d96aaf9e368437ad3900a5c7e58e3d23e94fa
Abraham Mughal <abmughal@cisco.com>
2024-06-06 11:07:11 -0700

    Removed unecessary import

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -20,6 +20,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+
 	// temporary unused until muxing is removed
 	// "github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
