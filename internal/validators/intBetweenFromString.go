2a73a49c96aaffa0869e8b946fb2a57233b65ed6
Gaspard Micol <gmicol@cisco.com>
2024-08-12 13:14:45 -0400

    [ignore] Modify inBetweenFromString validator from int to float64 comparison.

--- internal/validators/intBetweenFromString.go
+++ internal/validators/intBetweenFromString.go
@@ -13,7 +13,7 @@ import (
 var _ validator.String = InBetweenFromStringValidator{}
 
 type InBetweenFromStringValidator struct {
-	min, max int
+	min, max float64
 }
 
 func (v InBetweenFromStringValidator) Description(ctx context.Context) string {
@@ -21,30 +21,30 @@ func (v InBetweenFromStringValidator) Description(ctx context.Context) string {
 }
 
 func (v InBetweenFromStringValidator) MarkdownDescription(_ context.Context) string {
-	return fmt.Sprintf("value must be between %d and %d", v.min, v.max)
+	return fmt.Sprintf("value must be between %.2f and %.2f", v.min, v.max)
 }
 
 func (v InBetweenFromStringValidator) ValidateString(ctx context.Context, request validator.StringRequest, response *validator.StringResponse) {
 	if request.ConfigValue.IsNull() || request.ConfigValue.IsUnknown() {
 		return
 	}
-	configValueInt, err := strconv.Atoi(request.ConfigValue.ValueString())
+	configValueFloat, err := strconv.ParseFloat(request.ConfigValue.ValueString(), 64)
 	if err != nil {
 		response.Diagnostics.Append(validatordiag.InvalidAttributeValueDiagnostic(
 			request.Path,
 			err.Error(),
 			request.ConfigValue.ValueString(),
 		))
-	} else if configValueInt < int(v.min) || configValueInt > int(v.max) {
+	} else if configValueFloat < float64(v.min) || configValueFloat > float64(v.max) {
 		response.Diagnostics.Append(validatordiag.InvalidAttributeValueDiagnostic(
 			request.Path,
 			v.Description(ctx),
-			fmt.Sprintf("%d", configValueInt),
+			fmt.Sprintf("%.2f", configValueFloat),
 		))
 	}
 }
 
-func InBetweenFromString(min, max int) validator.String {
+func InBetweenFromString(min, max float64) validator.String {
 	if min > max {
 		return nil
 	}
