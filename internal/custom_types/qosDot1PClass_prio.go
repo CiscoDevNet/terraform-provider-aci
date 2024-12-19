820339a8322fea58d42a24ed55ee6299c6835197
akinross <akinross@cisco.com>
2024-12-19 10:14:40 +0100

    [minor_change] Add resources and datasources to support qosDot1PClass and qosDscpClass and expose them in aci_custom_qos_policy resource and datasource as children

---
+++ internal/custom_types/qosDot1PClass_prio.go
@@ -0,0 +1,159 @@
+package customTypes
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+)
+
+// QosDot1PClassPrio custom string type.
+
+var _ basetypes.StringTypable = QosDot1PClassPrioStringType{}
+
+type QosDot1PClassPrioStringType struct {
+	basetypes.StringType
+}
+
+func (t QosDot1PClassPrioStringType) Equal(o attr.Type) bool {
+	other, ok := o.(QosDot1PClassPrioStringType)
+
+	if !ok {
+		return false
+	}
+
+	return t.StringType.Equal(other.StringType)
+}
+
+func (t QosDot1PClassPrioStringType) String() string {
+	return "QosDot1PClassPrioStringType"
+}
+
+func (t QosDot1PClassPrioStringType) ValueFromString(ctx context.Context, in basetypes.StringValue) (basetypes.StringValuable, diag.Diagnostics) {
+	value := QosDot1PClassPrioStringValue{
+		StringValue: in,
+	}
+
+	return value, nil
+}
+
+func (t QosDot1PClassPrioStringType) ValueFromTerraform(ctx context.Context, in tftypes.Value) (attr.Value, error) {
+	attrValue, err := t.StringType.ValueFromTerraform(ctx, in)
+
+	if err != nil {
+		return nil, err
+	}
+
+	stringValue, ok := attrValue.(basetypes.StringValue)
+
+	if !ok {
+		return nil, fmt.Errorf("unexpected value type of %T", attrValue)
+	}
+
+	stringValuable, diags := t.ValueFromString(ctx, stringValue)
+
+	if diags.HasError() {
+		return nil, fmt.Errorf("unexpected error converting StringValue to StringValuable: %v", diags)
+	}
+
+	return stringValuable, nil
+}
+
+func (t QosDot1PClassPrioStringType) ValueType(ctx context.Context) attr.Value {
+	return QosDot1PClassPrioStringValue{}
+}
+
+// QosDot1PClassPrio custom string value.
+
+var _ basetypes.StringValuableWithSemanticEquals = QosDot1PClassPrioStringValue{}
+
+type QosDot1PClassPrioStringValue struct {
+	basetypes.StringValue
+}
+
+func (v QosDot1PClassPrioStringValue) Equal(o attr.Value) bool {
+	other, ok := o.(QosDot1PClassPrioStringValue)
+
+	if !ok {
+		return false
+	}
+
+	return v.StringValue.Equal(other.StringValue)
+}
+
+func (v QosDot1PClassPrioStringValue) Type(ctx context.Context) attr.Type {
+	return QosDot1PClassPrioStringType{}
+}
+
+func (v QosDot1PClassPrioStringValue) StringSemanticEquals(ctx context.Context, newValuable basetypes.StringValuable) (bool, diag.Diagnostics) {
+	var diags diag.Diagnostics
+
+	newValue, ok := newValuable.(QosDot1PClassPrioStringValue)
+
+	if !ok {
+		diags.AddError(
+			"Semantic Equality Check Error",
+			"An unexpected value type was received while performing semantic equality checks. "+
+				"Please report this to the provider developers.\n\n"+
+				"Expected Value Type: "+fmt.Sprintf("%T", v)+"\n"+
+				"Got Value Type: "+fmt.Sprintf("%T", newValuable),
+		)
+
+		return false, diags
+	}
+
+	priorMappedValue := QosDot1PClassPrioValueMap(v.StringValue)
+
+	newMappedValue := QosDot1PClassPrioValueMap(newValue.StringValue)
+
+	return priorMappedValue.Equal(newMappedValue), diags
+}
+
+func (v QosDot1PClassPrioStringValue) NamedValueString() string {
+	return QosDot1PClassPrioValueMap(basetypes.NewStringValue(v.ValueString())).ValueString()
+}
+
+func QosDot1PClassPrioValueMap(value basetypes.StringValue) basetypes.StringValue {
+	matchMap := map[string]string{
+		"0": "unspecified",
+		"1": "level3",
+		"2": "level2",
+		"3": "level1",
+		"7": "level6",
+		"8": "level5",
+		"9": "level4",
+	}
+
+	if val, ok := matchMap[value.ValueString()]; ok {
+		return basetypes.NewStringValue(val)
+	}
+
+	return value
+}
+
+func NewQosDot1PClassPrioStringNull() QosDot1PClassPrioStringValue {
+	return QosDot1PClassPrioStringValue{
+		StringValue: basetypes.NewStringNull(),
+	}
+}
+
+func NewQosDot1PClassPrioStringUnknown() QosDot1PClassPrioStringValue {
+	return QosDot1PClassPrioStringValue{
+		StringValue: basetypes.NewStringUnknown(),
+	}
+}
+
+func NewQosDot1PClassPrioStringValue(value string) QosDot1PClassPrioStringValue {
+	return QosDot1PClassPrioStringValue{
+		StringValue: basetypes.NewStringValue(value),
+	}
+}
+
+func NewQosDot1PClassPrioStringPointerValue(value *string) QosDot1PClassPrioStringValue {
+	return QosDot1PClassPrioStringValue{
+		StringValue: basetypes.NewStringPointerValue(value),
+	}
+}
