c353ba2f893c562e5f4b63670aaf45f00a44ef04
akinross <akinross@cisco.com>
2025-03-14 11:14:35 +0100

    [ignore] remove legacy ap resource and datasource files

--- aci/provider.go
+++ aci/provider.go
@@ -78,7 +78,6 @@ func Provider() *schema.Provider {
 
 		ResourcesMap: map[string]*schema.Resource{
 			"aci_tenant":                                   resourceAciTenant(),
-			"aci_application_profile":                      resourceAciApplicationProfile(),
 			"aci_contract":                                 resourceAciContract(),
 			"aci_contract_subject":                         resourceAciContractSubject(),
 			"aci_contract_subject_filter":                  resourceAciSubjectFilter(),
@@ -321,7 +320,6 @@ func Provider() *schema.Provider {
 
 		DataSourcesMap: map[string]*schema.Resource{
 			"aci_tenant":                                   dataSourceAciTenant(),
-			"aci_application_profile":                      dataSourceAciApplicationProfile(),
 			"aci_contract":                                 dataSourceAciContract(),
 			"aci_contract_subject":                         dataSourceAciContractSubject(),
 			"aci_contract_subject_filter":                  dataSourceAciSubjectFilter(),
