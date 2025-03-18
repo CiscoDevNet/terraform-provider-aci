61e3a55abca4ae43b70c834bc81d4b21f6532f16
akinross <akinross@cisco.com>
2025-03-18 09:17:25 +0100

    [ignore] add warning in documentation for migration impact that refers to migration docs

--- gen/generator.go
+++ gen/generator.go
@@ -1489,7 +1489,6 @@ func (m *Model) setClassModel(metaPath string, isChildIteration bool, definition
 		if len(parents) != 0 {
 			m.SetParentName(parents)
 		}
-		m.SetResourceNotesAndWarnigns(m.PkgName, definitions)
 		m.SetResourceNameAsDescription(m.PkgName, definitions)
 		m.SetTestType(classDetails, definitions)
 		m.SetTestApplicableFromVersion(classDetails)
@@ -1550,6 +1549,7 @@ func (m *Model) setClassModel(metaPath string, isChildIteration bool, definition
 	}
 
 	m.SetStateUpgradeTypeChanges(definitions)
+	m.SetResourceNotesAndWarnigns(m.PkgName, definitions)
 
 }
 
@@ -1843,6 +1843,13 @@ func (m *Model) SetResourceNotesAndWarnigns(classPkgName string, definitions Def
 				}
 			}
 		}
+	}
+
+	if len(m.LegacyAttributes) > 0 {
+		m.ResourceWarnings = append(
+			m.ResourceWarnings,
+			"This resource has been migrated to the terraform plugin protocol version 6, refer to the [migration guide](https://registry.terraform.io/providers/CiscoDevNet/aci/latest/docs/guides/migration) for more details and implications for already managed resources.",
+		)
 	}
 }
 
