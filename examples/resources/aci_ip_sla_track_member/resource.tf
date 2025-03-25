f01405b3745701d3dfa1b5efc03eeda6ba25a5d5
akinross <akinross@cisco.com>
2025-03-25 11:47:32 +0100

    [ignore] Overwrite scope and destination_ip_address attributes of fvTrackmember to be required as this is the case in UI

--- examples/resources/aci_ip_sla_track_member/resource.tf
+++ examples/resources/aci_ip_sla_track_member/resource.tf
@@ -1,5 +1,7 @@
 
 resource "aci_ip_sla_track_member" "example_tenant" {
-  parent_dn = aci_tenant.example.id
-  name      = "test_name"
+  parent_dn              = aci_tenant.example.id
+  destination_ip_address = "1.1.1.1"
+  name                   = "test_name"
+  scope                  = "uni/tn-test_tenant/BD-test_bd"
 }
