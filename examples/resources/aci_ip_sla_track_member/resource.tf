d7cdf0f08294a63f8e5b455c70d90846fb1debae
akinross <akinross@cisco.com>
2025-03-26 15:29:24 +0100

    [ignore] Add overwrite logic for example values

--- examples/resources/aci_ip_sla_track_member/resource.tf
+++ examples/resources/aci_ip_sla_track_member/resource.tf
@@ -3,7 +3,7 @@ resource "aci_ip_sla_track_member" "example_tenant" {
   parent_dn              = aci_tenant.example.id
   destination_ip_address = "1.1.1.1"
   name                   = "test_name"
-  scope                  = "uni/tn-test_tenant/BD-test_bd"
+  scope                  = aci_bridge_domain.example.id
   relation_to_monitoring_policy = {
     target_dn = aci_ip_sla_monitoring_policy.example.id
   }
