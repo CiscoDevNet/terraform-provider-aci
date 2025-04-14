611cd68edc45596cd1a7341843ee0cbf47103f50
akinross <akinross@cisco.com>
2025-04-14 15:40:49 +0200

    [minor_change] Migrate aci_taboo_contract and add support for taboo contract subjects vzTSubj

---
+++ examples/data-sources/aci_taboo_contract/data-source.tf
@@ -0,0 +1,5 @@
+
+data "aci_taboo_contract" "example_tenant" {
+  parent_dn = aci_tenant.example.id
+  name      = "test_name"
+}
---
+++ examples/resources/aci_relation_from_taboo_contract_to_filter/resource.tf
@@ -0,0 +1,5 @@
+
+resource "aci_relation_from_taboo_contract_to_filter" "example_taboo_contract_subject" {
+  parent_dn   = aci_taboo_contract_subject.example.id
+  filter_name = aci_filter.example.name
+}
