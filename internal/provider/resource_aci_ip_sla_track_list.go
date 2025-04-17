abc426d3ef80e5af319c1201a150a7b625311fd5
akinross <akinross@cisco.com>
2025-04-17 11:42:40 +0200

    [ignore] inline code comment change for resource_aci_ip_sla_track_list

--- internal/provider/resource_aci_ip_sla_track_list.go
+++ internal/provider/resource_aci_ip_sla_track_list.go
@@ -139,7 +139,7 @@ var FvRsOtmListMemberFvTrackListType = types.ObjectType{
 }
 
 func FvRsOtmListMemberFvTrackListSetToSetNullWhenStateIsNullPlanIsUnknownDuringUpdate(ctx context.Context, planValue, stateValue types.Set) basetypes.SetValue {
-	//  Function is needed to handle the case that an attribute is not yet suppored in a version and gets set to null during read
+	//  Function is needed to handle the case that an attribute is not yet supported in a version and gets set to null during read
 	var planSetValues, stateSetValues []FvRsOtmListMemberFvTrackListResourceModel
 	stateValue.ElementsAs(ctx, &stateSetValues, false)
 	planValue.ElementsAs(ctx, &planSetValues, false)
