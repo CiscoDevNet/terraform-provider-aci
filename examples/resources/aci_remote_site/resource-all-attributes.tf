0aeb4eb6326a5c56c769b4aed8b76a0faae4cd33
akinross <akinross@cisco.com>
2025-04-18 21:12:32 +0200

    [minor_change] Add resources and datasources for multisite translation mapping classes fvSiteAssociated and fvRemoteId

---
+++ examples/resources/aci_remote_site/resource-all-attributes.tf
@@ -0,0 +1,25 @@
+
+resource "aci_remote_site" "full_example_associated_site" {
+  parent_dn         = aci_associated_site.example.id
+  annotation        = "annotation"
+  description       = "description_1"
+  name              = "name_1"
+  name_alias        = "name_alias_1"
+  owner_key         = "owner_key_1"
+  owner_tag         = "owner_tag_1"
+  remote_vrf_pc_tag = "any"
+  remote_pc_tag     = "16387"
+  site_id           = "0"
+  annotations = [
+    {
+      key   = "key_0"
+      value = "value_1"
+    }
+  ]
+  tags = [
+    {
+      key   = "key_0"
+      value = "value_1"
+    }
+  ]
+}
