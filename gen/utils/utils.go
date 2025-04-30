7fe09bead9eebea926290541ef51763dc8f70901
akinross <akinross@cisco.com>
2025-04-30 11:51:01 +0200

    [ignore] Add functionality to GetFileNamesFromDirectorytest ro remove the file extension from the fiel name

--- gen/utils/utils.go
+++ gen/utils/utils.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/CiscoDevNet/terraform-provider-aci/v2/gen/utils/logger"
 )
@@ -10,7 +12,7 @@ import (
 // Initialize a logger instance for the generator.
 var genLogger = logger.InitalizeLogger()
 
-func GetFileNamesFromDirectory(path string) []string {
+func GetFileNamesFromDirectory(path string, removeExtension bool) []string {
 	genLogger.Debug(fmt.Sprintf("Getting file names from directory: %s.", path))
 	var names []string
 	entries, err := os.ReadDir(path)
@@ -18,7 +20,13 @@ func GetFileNamesFromDirectory(path string) []string {
 		for _, entry := range entries {
 			// Check if the entry is not a directory and append its name to the list.
 			if !entry.IsDir() {
-				names = append(names, entry.Name())
+				name := entry.Name()
+				// When removeExtension is true, remove the file extension from the name. (ex file1.json -> file1)
+				// When removeExtension is false, keep the file extension in the name. (ex file1.json -> file1.json)
+				if removeExtension {
+					name = strings.TrimSuffix(filepath.Base(name), filepath.Ext(name))
+				}
+				names = append(names, name)
 			}
 		}
 	}
