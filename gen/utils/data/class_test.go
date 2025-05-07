199b0f7e4a04f258307bd69307991a49d154a579
akinross <akinross@cisco.com>
2025-05-07 08:58:50 +0200

    [ignore] avoid fatal logging and return errors

--- gen/utils/data/class_test.go
+++ gen/utils/data/class_test.go
@@ -11,18 +11,29 @@ import (
 const (
 	constTestClassNameSingleWordInShortName    = "fvTenant"
 	constTestClassNameMultipleWordsInShortName = "fvRsIpslaMonPol"
+	constTestClassNameErrorInShortName         = "error"
 )
 
 func TestSplitClassNameToPackageNameAndShortNameSingle(t *testing.T) {
 	test.InitializeTest(t, 0)
-	packageName, shortName := splitClassNameToPackageNameAndShortName(constTestClassNameSingleWordInShortName)
+	packageName, shortName, err := splitClassNameToPackageNameAndShortName(constTestClassNameSingleWordInShortName)
 	assert.Equal(t, packageName, "fv", fmt.Sprintf("Expected package name to be 'fv', but got '%s'", packageName))
 	assert.Equal(t, shortName, "Tenant", fmt.Sprintf("Expected short name to be 'Tenant', but got '%s'", shortName))
+	assert.NoError(t, err, fmt.Sprintf("Expected no error, but got '%s'", err))
 }
 
 func TestSplitClassNameToPackageNameAndShortNameMultiple(t *testing.T) {
 	test.InitializeTest(t, 0)
-	packageName, shortName := splitClassNameToPackageNameAndShortName(constTestClassNameMultipleWordsInShortName)
+	packageName, shortName, err := splitClassNameToPackageNameAndShortName(constTestClassNameMultipleWordsInShortName)
 	assert.Equal(t, packageName, "fv", fmt.Sprintf("Expected package name to be 'fv', but got '%s'", packageName))
 	assert.Equal(t, shortName, "RsIpslaMonPol", fmt.Sprintf("Expected short name to be 'RsIpslaMonPol', but got '%s'", shortName))
+	assert.NoError(t, err, fmt.Sprintf("Expected no error, but got '%s'", err))
+}
+
+func TestSplitClassNameToPackageNameAndShortNameError(t *testing.T) {
+	test.InitializeTest(t, 0)
+	packageName, shortName, err := splitClassNameToPackageNameAndShortName(constTestClassNameErrorInShortName)
+	assert.Equal(t, packageName, "", fmt.Sprintf("Expected package name to be '', but got '%s'", packageName))
+	assert.Equal(t, shortName, "", fmt.Sprintf("Expected short name to be '', but got '%s'", shortName))
+	assert.Error(t, err, fmt.Sprintf("Expected error, but got '%s'", err))
 }
