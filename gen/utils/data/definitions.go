72a590ba1dfdb128b9ab74cad117a16b832e042c
akinross <akinross@cisco.com>
2025-06-03 14:08:26 +0200

    [ignore] add logic for reading a global definition file and adjust resource name setting for relationships

---
+++ gen/utils/data/definitions.go
@@ -0,0 +1,28 @@
+package data
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type GlobalMetaDefinition struct {
+	// A map containing class names as keys and their corresponding resource names as values.
+	// This is used search for the resource name of a class when it is not defined in meta directory.
+	NoMetaFile map[string]string
+}
+
+func loadGlobalMetaDefinition() GlobalMetaDefinition {
+	definition, err := os.ReadFile(constGlobalDefinitionFilePath)
+	if err != nil {
+		genLogger.Fatal("A file 'global.yaml' is required to be defined in the definitions folder.")
+	}
+
+	var definitionGlobalMetaData GlobalMetaDefinition
+	err = yaml.Unmarshal(definition, &definitionGlobalMetaData)
+	if err != nil {
+		genLogger.Fatal(err.Error())
+	}
+
+	return definitionGlobalMetaData
+}
