6f353bbeb9bcd32c778eb12b31dca8a7b321148a
akinross <akinross@cisco.com>
2025-12-03 20:38:02 +0100

    [minor_change] Migrate resource and datasource aci_spanning_tree_interface_policy to terraform plugin framework

---
+++ examples/data-sources/aci_spanning_tree_interface_policy/data-source.tf
@@ -0,0 +1,4 @@
+
+data "aci_spanning_tree_interface_policy" "example" {
+  name = "test_name"
+}
---
+++ examples/resources/aci_spanning_tree_interface_policy/resource.tf
@@ -0,0 +1,4 @@
+
+resource "aci_spanning_tree_interface_policy" "example" {
+  name = "test_name"
+}
