9b819d8ee96f6fabcc0c131c70d0b4be47834867
akinross <akinross@cisco.com>
2026-04-22 13:17:34 +0200

    [ignore] add versions and deprecation to property struct

--- gen/utils/data/property_test.go
+++ gen/utils/data/property_test.go
@@ -470,3 +470,259 @@ func TestSetSensitive(t *testing.T) {
 		})
 	}
 }
+
+func TestPropertySetDeprecated(t *testing.T) {
+	t.Parallel()
+	test.InitializeTest(t)
+
+	testCases := []test.TestCase{
+		{
+			Name:     "test_deprecated_false",
+			Input:    PropertyDefinition{},
+			Expected: false,
+		},
+		{
+			Name:     "test_deprecated_true",
+			Input:    PropertyDefinition{Deprecated: true},
+			Expected: true,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			t.Parallel()
+
+			property := &Property{
+				PropertyName:       "testProp",
+				propertyDefinition: testCase.Input.(PropertyDefinition),
+			}
+
+			property.setDeprecated()
+
+			assert.Equal(t, testCase.Expected, property.Deprecated, test.MessageEqual(testCase.Expected, property.Deprecated, testCase.Name))
+		})
+	}
+}
+
+type setPropertyDeprecatedVersionsExpected struct {
+	Raw      string
+	String   string
+	Nil      bool
+	Error    bool
+	ErrorMsg string
+}
+
+func TestPropertySetDeprecatedVersions(t *testing.T) {
+	t.Parallel()
+	test.InitializeTest(t)
+
+	testCases := []test.TestCase{
+		{
+			Name:  "test_deprecated_versions_not_set",
+			Input: PropertyDefinition{},
+			Expected: setPropertyDeprecatedVersionsExpected{
+				Nil: true,
+			},
+		},
+		{
+			Name:  "test_deprecated_versions_single_range",
+			Input: PropertyDefinition{DeprecatedVersions: "4.2(7f)-"},
+			Expected: setPropertyDeprecatedVersionsExpected{
+				Raw:    "4.2(7f)-",
+				String: "4.2(7f) and later",
+			},
+		},
+		{
+			Name:  "test_deprecated_versions_bounded_range",
+			Input: PropertyDefinition{DeprecatedVersions: "3.2(10e)-4.2(7f)"},
+			Expected: setPropertyDeprecatedVersionsExpected{
+				Raw:    "3.2(10e)-4.2(7f)",
+				String: "3.2(10e) to 4.2(7f)",
+			},
+		},
+		{
+			Name:  "test_deprecated_versions_multiple_ranges",
+			Input: PropertyDefinition{DeprecatedVersions: "3.2(10e)-3.2(10g),4.2(7f)-"},
+			Expected: setPropertyDeprecatedVersionsExpected{
+				Raw:    "3.2(10e)-3.2(10g),4.2(7f)-",
+				String: "3.2(10e) to 3.2(10g), 4.2(7f) and later",
+			},
+		},
+		{
+			Name:  "test_error_invalid_deprecated_version",
+			Input: PropertyDefinition{DeprecatedVersions: "invalid"},
+			Expected: setPropertyDeprecatedVersionsExpected{
+				Error:    true,
+				ErrorMsg: "failed to parse deprecated versions for property 'testProp': invalid version 'invalid': unknown",
+			},
+		},
+		{
+			Name:  "test_error_invalid_deprecated_version_in_range",
+			Input: PropertyDefinition{DeprecatedVersions: "4.2(7f)-,invalid"},
+			Expected: setPropertyDeprecatedVersionsExpected{
+				Error:    true,
+				ErrorMsg: "failed to parse deprecated versions for property 'testProp': invalid version 'invalid': unknown",
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			t.Parallel()
+			expected := testCase.Expected.(setPropertyDeprecatedVersionsExpected)
+
+			property := &Property{
+				PropertyName:       "testProp",
+				propertyDefinition: testCase.Input.(PropertyDefinition),
+			}
+
+			err := property.setDeprecatedVersions()
+
+			if expected.Error {
+				assert.EqualError(t, err, expected.ErrorMsg)
+			} else if expected.Nil {
+				assert.NoError(t, err, test.MessageUnexpectedError(err))
+				assert.Nil(t, property.DeprecatedVersions, "expected DeprecatedVersions to be nil")
+			} else {
+				assert.NoError(t, err, test.MessageUnexpectedError(err))
+				assert.Equal(t, expected.Raw, property.DeprecatedVersions.Raw(), test.MessageEqual(expected.Raw, property.DeprecatedVersions.Raw(), testCase.Name))
+				assert.Equal(t, expected.String, property.DeprecatedVersions.String(), test.MessageEqual(expected.String, property.DeprecatedVersions.String(), testCase.Name))
+			}
+		})
+	}
+}
+
+type setPropertySupportedVersionsInput struct {
+	MetaDetails        map[string]interface{}
+	PropertyDefinition PropertyDefinition
+}
+
+type setPropertySupportedVersionsExpected struct {
+	Raw      string
+	String   string
+	Nil      bool
+	Error    bool
+	ErrorMsg string
+}
+
+func TestPropertySetSupportedVersions(t *testing.T) {
+	t.Parallel()
+	test.InitializeTest(t)
+
+	testCases := []test.TestCase{
+		{
+			Name: "test_versions_not_set",
+			Input: setPropertySupportedVersionsInput{
+				MetaDetails:        map[string]interface{}{},
+				PropertyDefinition: PropertyDefinition{},
+			},
+			Expected: setPropertySupportedVersionsExpected{
+				Nil: true,
+			},
+		},
+		{
+			Name: "test_versions_from_meta_file",
+			Input: setPropertySupportedVersionsInput{
+				MetaDetails:        map[string]interface{}{"versions": "4.2(7f)-"},
+				PropertyDefinition: PropertyDefinition{},
+			},
+			Expected: setPropertySupportedVersionsExpected{
+				Raw:    "4.2(7f)-",
+				String: "4.2(7f) and later",
+			},
+		},
+		{
+			Name: "test_versions_from_definition_override",
+			Input: setPropertySupportedVersionsInput{
+				MetaDetails:        map[string]interface{}{"versions": "4.2(7f)-"},
+				PropertyDefinition: PropertyDefinition{SupportedVersions: "5.0(1a)-"},
+			},
+			Expected: setPropertySupportedVersionsExpected{
+				Raw:    "5.0(1a)-",
+				String: "5.0(1a) and later",
+			},
+		},
+		{
+			Name: "test_versions_from_definition_when_meta_empty",
+			Input: setPropertySupportedVersionsInput{
+				MetaDetails:        map[string]interface{}{},
+				PropertyDefinition: PropertyDefinition{SupportedVersions: "5.0(1a)-"},
+			},
+			Expected: setPropertySupportedVersionsExpected{
+				Raw:    "5.0(1a)-",
+				String: "5.0(1a) and later",
+			},
+		},
+		{
+			Name: "test_bounded_range",
+			Input: setPropertySupportedVersionsInput{
+				MetaDetails:        map[string]interface{}{"versions": "3.2(10e)-4.2(7f)"},
+				PropertyDefinition: PropertyDefinition{},
+			},
+			Expected: setPropertySupportedVersionsExpected{
+				Raw:    "3.2(10e)-4.2(7f)",
+				String: "3.2(10e) to 4.2(7f)",
+			},
+		},
+		{
+			Name: "test_multiple_ranges",
+			Input: setPropertySupportedVersionsInput{
+				MetaDetails:        map[string]interface{}{"versions": "3.2(10e)-3.2(10g),4.2(7f)-"},
+				PropertyDefinition: PropertyDefinition{},
+			},
+			Expected: setPropertySupportedVersionsExpected{
+				Raw:    "3.2(10e)-3.2(10g),4.2(7f)-",
+				String: "3.2(10e) to 3.2(10g), 4.2(7f) and later",
+			},
+		},
+		{
+			Name: "test_error_invalid_version",
+			Input: setPropertySupportedVersionsInput{
+				MetaDetails:        map[string]interface{}{"versions": "invalid"},
+				PropertyDefinition: PropertyDefinition{},
+			},
+			Expected: setPropertySupportedVersionsExpected{
+				Error:    true,
+				ErrorMsg: "failed to parse versions for property 'testProp': invalid version 'invalid': unknown",
+			},
+		},
+		{
+			Name: "test_error_invalid_version_in_range",
+			Input: setPropertySupportedVersionsInput{
+				MetaDetails:        map[string]interface{}{"versions": "4.2(7f)-,invalid"},
+				PropertyDefinition: PropertyDefinition{},
+			},
+			Expected: setPropertySupportedVersionsExpected{
+				Error:    true,
+				ErrorMsg: "failed to parse versions for property 'testProp': invalid version 'invalid': unknown",
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			t.Parallel()
+			input := testCase.Input.(setPropertySupportedVersionsInput)
+			expected := testCase.Expected.(setPropertySupportedVersionsExpected)
+
+			property := &Property{
+				PropertyName:       "testProp",
+				metaDetails:        input.MetaDetails,
+				propertyDefinition: input.PropertyDefinition,
+			}
+
+			err := property.setSupportedVersions()
+
+			if expected.Error {
+				assert.EqualError(t, err, expected.ErrorMsg)
+			} else if expected.Nil {
+				assert.NoError(t, err, test.MessageUnexpectedError(err))
+				assert.Nil(t, property.SupportedVersions, "expected SupportedVersions to be nil")
+			} else {
+				assert.NoError(t, err, test.MessageUnexpectedError(err))
+				assert.Equal(t, expected.Raw, property.SupportedVersions.Raw(), test.MessageEqual(expected.Raw, property.SupportedVersions.Raw(), testCase.Name))
+				assert.Equal(t, expected.String, property.SupportedVersions.String(), test.MessageEqual(expected.String, property.SupportedVersions.String(), testCase.Name))
+			}
+		})
+	}
+}
